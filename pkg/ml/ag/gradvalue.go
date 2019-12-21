8c626b2b823e0b16b1f7b0c77970a2449a2b9955
matteo-grella <matteogrella@gmail.com>
2019-12-21 10:14:03 +0100

    Define GradValue interface

---
+++ pkg/ml/ag/gradvalue.go
@@ -0,0 +1,25 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ag
+
+import "brillion.io/spago/pkg/mat"
+
+type GradValue interface {
+	// Value returns the value of the node.
+	// If the node is a variable it returns its value, otherwise returns the cached result of the forward pass.
+	Value() mat.Matrix
+	// ScalarValue() returns the scalar value of the node. It panics if the value is not a scalar.
+	ScalarValue() float64
+	// Grad returns the gradients accumulated during the backward pass.
+	Grad() mat.Matrix
+	// HasGrad returns true if there are accumulated gradients.
+	HasGrad() bool
+	// RequiresGrad returns true if the node requires gradients.
+	RequiresGrad() bool
+	// PropagateGrad propagates the gradients to the node.
+	PropagateGrad(gx mat.Matrix)
+	// ZeroGrad set the gradients to zeros.
+	ZeroGrad()
+}
