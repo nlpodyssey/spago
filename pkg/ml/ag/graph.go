a97da6bb4d6569a083715996e59fc707eacdd301
matteo-grella <matteogrella@gmail.com>
2019-12-21 16:52:30 +0100

    Add NewVariable, NewScalar, NewOperator, NewWrap constructors

--- pkg/ml/ag/graph.go
+++ pkg/ml/ag/graph.go
@@ -5,7 +5,10 @@
 package ag
 
 import (
+	"brillion.io/spago/pkg/mat"
+	"brillion.io/spago/pkg/ml/ag/fn"
 	"sync"
+	"sync/atomic"
 )
 
 type Graph struct {
@@ -42,4 +45,155 @@ func (g *Graph) Reset() {
 	g.maxId = 0
 	g.maxDepth = 0
 	g.nodes = make([]*nodeInfo, 0)
+}
+
+// NewVariable creates e returns a new node.
+func (g *Graph) NewVariable(value mat.Matrix, requiresGrad bool) *Variable {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	newId := g.newId()
+	newNode := &Variable{
+		graph:        g,
+		id:           newId,
+		value:        value,
+		grad:         nil,
+		hasGrad:      false,
+		requiresGrad: requiresGrad,
+	}
+	// the new id is sequential so this the append is fine
+	g.nodes = append(g.nodes, &nodeInfo{
+		node:        newNode,
+		depth:       0,
+		descendants: []int64{newId},
+	})
+	return newNode
+}
+
+// NewScalar creates a variable node that doesn't require gradients
+func (g *Graph) NewScalar(value float64) *Variable {
+	return g.NewVariable(mat.NewScalar(value), false)
+}
+
+// NewOperator creates a new operator along with its forward pass.
+func (g *Graph) NewOperator(f fn.Function, operands ...Node) *Operator {
+	value := f.Forward() // the calculation can be concurrent
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	newId := g.newId()
+	newNode := &Operator{
+		graph:        g,
+		id:           newId,
+		function:     f,
+		value:        value,
+		grad:         nil,
+		hasGrad:      false,
+		requiresGrad: requireGrad(operands),
+	}
+
+	descendants := make([]int64, 0, g.sumDescendants(operands)+1) // + itself
+	mark := make([]bool, len(g.nodes), len(g.nodes))
+	for _, o := range operands {
+		for _, descendantId := range g.nodes[o.Id()].descendants {
+			if !mark[descendantId] {
+				mark[descendantId] = true
+				g.nodes[descendantId].depth++
+				g.maxDepth = maxDepth(g.nodes[descendantId].depth, g.maxDepth)
+				descendants = append(descendants, descendantId)
+			}
+		}
+	}
+	descendants = append(descendants, newId)
+
+	// the new id is sequential so this the append is fine
+	g.nodes = append(g.nodes, &nodeInfo{
+		node:        newNode,
+		depth:       0,
+		descendants: descendants,
+	})
+	return newNode
+}
+
+func (g *Graph) NewWrap(value GradValue) *Wrapper {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	newId := g.newId()
+	newNode := &Wrapper{
+		GradValue: value,
+		graph:     g,
+		id:        newId,
+		wrapGrad:  true,
+	}
+	// the new id is sequential so this the append is fine
+	g.nodes = append(g.nodes, &nodeInfo{
+		node:        newNode,
+		depth:       0,
+		descendants: []int64{newId},
+	})
+	return newNode
+}
+
+func (g *Graph) NewWrapNoGrad(value GradValue) *Wrapper {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	newId := g.newId()
+	newNode := &Wrapper{
+		GradValue: value,
+		graph:     g,
+		id:        newId,
+		wrapGrad:  false,
+	}
+	// the new id is sequential so this the append is fine
+	g.nodes = append(g.nodes, &nodeInfo{
+		node:        newNode,
+		depth:       0,
+		descendants: []int64{newId},
+	})
+	return newNode
+}
+
+// newId generates and returns a new incremental sequential ID.
+func (g *Graph) newId() int64 {
+	return atomic.AddInt64(&g.maxId, 1) - 1
+}
+
+func (g *Graph) sumDescendants(ns []Node) int {
+	sum := 0
+	for _, n := range ns {
+		sum += len(g.nodes[n.Id()].descendants)
+	}
+	return sum
+}
+
+// groupNodesByDepth returns the nodes of the graph grouped by depth.
+func (g *Graph) groupNodesByDepth() [][]Node {
+	out := make([][]Node, g.maxDepth+1)
+	for _, n := range g.nodes {
+		out[n.depth] = append(out[n.depth], n.node)
+	}
+	return out
+}
+
+func maxDepth(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func requireGrad(ns []Node) bool {
+	for _, n := range ns {
+		if n.RequiresGrad() {
+			return true
+		}
+	}
+	return false
+}
+
+// nodesToGradValues converts a slice of node to a slice of operands.
+func nodesToGradValues(xs []Node) []fn.Operand {
+	var out = make([]fn.Operand, len(xs))
+	for i, x := range xs {
+		out[i] = x
+	}
+	return out
 }
