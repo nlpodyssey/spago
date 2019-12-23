6178991af019b7e05bcc8a002c85942b1bead220
matteo-grella <matteogrella@gmail.com>
2019-12-23 17:44:28 +0100

    Implement exponential and hyperbolic decay

---
+++ pkg/ml/optimizers/gd/decay/exponential/exponential.go
@@ -0,0 +1,34 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package exponential
+
+import "math"
+
+// Exponential defines an exponential decay depending on the time step:
+//     lr = exp((times - t) * log(lr) + log(final))
+type Exponential struct {
+	init  float64
+	final float64
+	times int
+}
+
+func New(init, final float64, iter int) *Exponential {
+	if init < final {
+		panic("decay: the initial learning rate must be >= than the final one")
+	}
+	return &Exponential{
+		init:  init,
+		final: final,
+		times: iter,
+	}
+}
+
+func (d *Exponential) Decay(lr float64, t int) float64 {
+	if t > 1 && lr > d.final {
+		return math.Exp((float64(d.times-t)*math.Log(lr) + math.Log(d.final)) / float64(d.times-t+1))
+	} else {
+		return lr
+	}
+}
---
+++ pkg/ml/optimizers/gd/decay/hyperbolic/hyperbolic.go
@@ -0,0 +1,32 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package hyperbolic
+
+// Hyperbolic defines an hyperbolic decay depending on the time step
+//     lr = lr / (1 + rate*t).
+type Hyperbolic struct {
+	init  float64
+	final float64
+	rate  float64
+}
+
+func New(init, final, rate float64) *Hyperbolic {
+	if init < final {
+		panic("decay: the initial learning rate must be >= than the final one")
+	}
+	return &Hyperbolic{
+		init:  init,
+		final: final,
+		rate:  rate,
+	}
+}
+
+func (d *Hyperbolic) Decay(lr float64, t int) float64 {
+	if t > 1 && d.rate > 0.0 && lr > d.final {
+		return d.init / (1.0 + d.rate*float64(t))
+	} else {
+		return lr
+	}
+}
