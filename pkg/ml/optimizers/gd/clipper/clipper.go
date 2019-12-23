17f3b56f750fedfb621e4139c08716235f74111f
matteo-grella <matteogrella@gmail.com>
2019-12-23 12:18:00 +0100

    Add gradients clippers by Value and by Norm

---
+++ pkg/ml/optimizers/gd/clipper/clipper.go
@@ -0,0 +1,56 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package clipper
+
+import (
+	"brillion.io/spago/pkg/mat"
+	"math"
+)
+
+type GradClipper interface {
+	Clip(gs []mat.Matrix)
+}
+
+type ClipValue struct {
+	value float64
+}
+
+func (c *ClipValue) Clip(gs []mat.Matrix) {
+	for _, g := range gs {
+		g.Clip(-c.value, c.value)
+	}
+}
+
+type ClipNorm struct {
+	MaxNorm, NormType float64
+}
+
+// ClipNorm clips the gradients, multiplying each parameter by the MaxNorm, divided by n-norm of the overall gradients.
+// NormType is the n-norm. Can be ``Double.POSITIVE_INFINITY`` for infinity norm (default 2.0)
+func (c *ClipNorm) Clip(gs []mat.Matrix) {
+	if c.NormType <= 1 {
+		panic("gd: norm type required to be > 1.")
+	}
+
+	totalNorm := 0.0
+	if math.IsInf(c.NormType, 1) {
+		for _, g := range gs {
+			totalNorm = math.Max(g.Abs().Max(), totalNorm)
+		}
+	} else {
+		sum := 0.0
+		for _, g := range gs {
+			sum += g.Abs().Pow(c.NormType).Sum()
+		}
+		totalNorm = math.Pow(sum, 1.0/c.NormType)
+	}
+
+	clipCoeff := c.MaxNorm / (totalNorm + 0.0000001)
+	if clipCoeff < 1.0 {
+		for _, g := range gs {
+			g.ProdScalarInPlace(clipCoeff)
+		}
+	}
+}
---
+++ pkg/ml/optimizers/gd/clipper/clipper_test.go
@@ -0,0 +1,76 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package clipper
+
+import (
+	"brillion.io/spago/pkg/mat"
+	"gonum.org/v1/gonum/floats"
+	"math"
+	"testing"
+)
+
+func TestClipValue(t *testing.T) {
+	gs := buildTestGrads()
+	(&ClipValue{value: 0.7}).Clip(gs)
+	if !floats.EqualApprox(gs[0].Data(), []float64{
+		0.5, 0.6, -0.7, -0.6,
+		0.7, -0.4, 0.1, -0.7,
+		0.7, -0.7, 0.3, 0.5,
+		0.7, -0.7, 0.0, -0.1,
+		0.4, 0.7, -0.7, 0.7,
+	}, 1.0e-05) {
+		t.Error("gd: gs[0] doesn't match the expected values")
+	}
+	if !floats.EqualApprox(gs[1].Data(), []float64{0.7, 0.7, 0.4, 0.7, 0.1}, 1.0e-05) {
+		t.Error("gd: gs[1] doesn't match the expected values")
+	}
+}
+
+func TestClip2Norm(t *testing.T) {
+	gs := buildTestGrads()
+	(&ClipNorm{MaxNorm: 2.0, NormType: 2.0}).Clip(gs)
+	if !floats.EqualApprox(gs[0].Data(), []float64{
+		0.314814, 0.377777, -0.503702, -0.377777,
+		0.440739, -0.251851, 0.062962, -0.503702,
+		0.440739, -0.440739, 0.188888, 0.314814,
+		0.503702, -0.566665, 0.0, -0.062962,
+		0.251851, 0.629628, -0.440739, 0.503702,
+	}, 1.0e-06) {
+		t.Error("gd: gs[0] doesn't match the expected values")
+	}
+	if !floats.EqualApprox(gs[1].Data(), []float64{0.566665, 0.440739, 0.251851, 0.503702, 0.062962}, 1.0e-06) {
+		t.Error("gd: gs[1] doesn't match the expected values")
+	}
+}
+
+func TestClipNormInf(t *testing.T) {
+	gs := buildTestGrads()
+	(&ClipNorm{MaxNorm: 0.5, NormType: math.Inf(+1)}).Clip(gs)
+	if !floats.EqualApprox(gs[0].Data(), []float64{
+		0.25, 0.3, -0.4, -0.3,
+		0.35, -0.2, 0.05, -0.4,
+		0.35, -0.35, 0.15, 0.25,
+		0.4, -0.45, 0.0, -0.05,
+		0.2, 0.5, -0.35, 0.4,
+	}, 1.0e-06) {
+		t.Error("gd: gs[0] doesn't match the expected values")
+	}
+	if !floats.EqualApprox(gs[1].Data(), []float64{0.45, 0.35, 0.2, 0.4, 0.05}, 1.0e-06) {
+		t.Error("gd: gs[1] doesn't match the expected values")
+	}
+}
+
+func buildTestGrads() []mat.Matrix {
+	return []mat.Matrix{
+		mat.NewDense(4, 5, []float64{
+			0.5, 0.6, -0.8, -0.6,
+			0.7, -0.4, 0.1, -0.8,
+			0.7, -0.7, 0.3, 0.5,
+			0.8, -0.9, 0.0, -0.1,
+			0.4, 1.0, -0.7, 0.8,
+		}),
+		mat.NewVecDense([]float64{0.9, 0.7, 0.4, 0.8, 0.1}),
+	}
+}
