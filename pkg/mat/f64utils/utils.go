896a9b66811eb0d4ce64b85fcef4661e9fc4ce1c
matteo-grella <matteogrella@gmail.com>
2019-12-24 13:03:49 +0100

    Add untracked math utils

---
+++ pkg/mat/f64utils/utils.go
@@ -0,0 +1,112 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package f64utils
+
+import (
+	"gonum.org/v1/gonum/floats"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func EqualApprox(a, b float64) bool {
+	return floats.EqualWithinAbsOrRel(a, b, 1.0e-06, 1.0e-06)
+}
+
+func Copy(in []float64) []float64 {
+	out := make([]float64, len(in))
+	copy(out, in)
+	return out
+}
+
+// Fill the given slice's elements with value
+func FillFloatSlice(slice []float64, value float64) {
+	for i := range slice {
+		slice[i] = value
+	}
+}
+
+func Max(v []float64) (m float64) {
+	m = v[len(v)-1]
+	for _, e := range v {
+		if m <= e {
+			m = e
+		}
+	}
+	return
+}
+
+func Sum(v []float64) (s float64) {
+	for _, e := range v {
+		s += e
+	}
+	return
+}
+
+// ArgMinMax finds the indices of min and max arguments
+func ArgMinMax(v []float64) (imin, imax int) {
+	if len(v) < 1 {
+		return
+	}
+	vmin, vmax := v[0], v[0]
+	imin, imax = 0, 0
+	for i := 1; i < len(v); i++ {
+		if v[i] < vmin {
+			imin = i
+			vmin = v[i]
+		}
+		if v[i] > vmax {
+			imax = i
+			vmax = v[i]
+		}
+	}
+	return
+}
+
+// ArgMax finds the index of the max argument
+func ArgMax(v []float64) int {
+	_, imax := ArgMinMax(v)
+	return imax
+}
+
+// ArgMin finds the index of the min argument
+func ArgMin(v []float64) int {
+	imin, _ := ArgMinMax(v)
+	return imin
+}
+
+func MakeFloat64Matrix(rows, cols int) [][]float64 {
+	matrix := make([][]float64, rows)
+	for i := 0; i < rows; i++ {
+		matrix[i] = make([]float64, cols)
+	}
+	return matrix
+}
+
+func StrToFloat64Slice(str string) ([]float64, error) {
+	spl := strings.Fields(str)
+	data := make([]float64, len(spl))
+	for i, v := range spl {
+		if num, err := strconv.ParseFloat(v, 64); err == nil {
+			data[i] = num
+		} else {
+			return nil, err
+		}
+	}
+	return data, nil
+}
+
+func SoftMax(v []float64) (sm []float64) {
+	c := Max(v)
+	var sum float64 = 0
+	for _, e := range v {
+		sum += math.Exp(e - c)
+	}
+	sm = make([]float64, len(v))
+	for i, v := range v {
+		sm[i] = math.Exp(v-c) / sum
+	}
+	return sm
+}
---
+++ pkg/mat/misc.go
@@ -0,0 +1,43 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mat
+
+import (
+	"fmt"
+	"math"
+)
+
+func EqualDims(a, b Matrix) bool {
+	r, c := a.Dims()
+	r2, c2 := b.Dims()
+	return r == r2 && c == c2
+}
+
+func Sqrt(m Matrix) Matrix {
+	buf := m.ZerosLike()
+	buf.Apply(func(i, j int, v float64) float64 {
+		return math.Sqrt(v)
+	}, m)
+	return buf
+}
+
+func Print(a Matrix) {
+	r, c := a.Dims()
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			fmt.Printf("%.8f ", a.At(i, j))
+		}
+	}
+	fmt.Printf("\n")
+}
+
+// Cosine returns the cosine similarity between two vectors.
+func Cosine(x, y Matrix) float64 {
+	d := x.DotUnitary(y)
+	xNorm := x.Norm(2.0)
+	yNorm := y.Norm(2.0)
+	return d / (xNorm * yNorm)
+}
+
