c180152b14283bc5c4f2b6387910f54c4946eca2
matteo-grella <matteogrella@gmail.com>
2019-12-27 10:48:36 +0100

    Add Conv2D transformation

--- pkg/ml/nn/transforms.go
+++ pkg/ml/nn/transforms.go
@@ -37,3 +37,27 @@ func Affine(g *ag.Graph, xs ...ag.Node) ag.Node {
 func BiLinear(g *ag.Graph, w, x1, x2 ag.Node) ag.Node {
 	return g.Mul(g.Mul(g.T(x1), w), x2)
 }
+
+// Conv2D performs a 2D convolution.
+func Conv2D(g *ag.Graph, w, x ag.Node, xStride, yStride int) ag.Node {
+	var dimx, dimy int
+	if (x.Value().Rows()-w.Value().Rows())%xStride != 0 {
+		panic("Incompatible stride value for rows")
+	}
+	if (x.Value().Columns()-w.Value().Columns())%yStride != 0 {
+		panic("Incompatible stride value for columns")
+	}
+	dimx = (x.Value().Rows()-w.Value().Rows())/xStride + 1
+	dimy = (x.Value().Columns()-w.Value().Columns())/yStride + 1
+
+	var outList []ag.Node
+	for i := 0; i < dimx; i++ {
+		for j := 0; j < dimy; j++ {
+			var view = g.View(x, i*xStride, j*yStride, w.Value().Rows(), w.Value().Columns())
+			var dotProduct = g.Dot(view, w)
+			outList = append(outList, dotProduct)
+		}
+	}
+
+	return g.Reshape(g.Concat(outList...), dimx, dimy)
+}
---
+++ pkg/ml/nn/transforms_test.go
@@ -0,0 +1,107 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package nn
+
+import (
+	"brillion.io/spago/pkg/mat"
+	"brillion.io/spago/pkg/ml/ag"
+	"gonum.org/v1/gonum/floats"
+	"testing"
+)
+
+func TestConv2D(t *testing.T) {
+	var g = ag.NewGraph()
+
+	x := g.NewVariable(mat.NewDense(4, 4, []float64{
+		0.2, 0.1, 0.5, 0.8,
+		0.4, -0.3, -0.2, -0.3,
+		0.5, -0.6, -0.4, 0.6,
+		-0.3, 0.9, 0.5, 0.5,
+	}), true)
+
+	w := g.NewVariable(mat.NewDense(2, 2, []float64{
+		0.5, -0.4,
+		0.3, 0.3,
+	}), true)
+
+	out := Conv2D(g, w, x, 1, 1)
+
+	if !floats.EqualApprox(out.Value().Data(), []float64{
+		0.09, -0.3, -0.22,
+		0.29, -0.37, 0.08,
+		0.67, 0.28, -0.14,
+	}, 0.005) {
+		t.Error("out value doesn't match the expected values")
+	}
+
+	g.Backward(out, mat.NewDense(3, 3, []float64{
+		1.0, -0.5, -1.0,
+		0.5, 0.3, 0.5,
+		0.2, 0.5, -0.5,
+	}))
+
+	if !floats.EqualApprox(w.Grad().Data(), []float64{
+		-0.34, -1.93,
+		0.76, 0.16,
+	}, 0.005) {
+		t.Error("w gradients don't match the expected values")
+	}
+
+	if !floats.EqualApprox(x.Grad().Data(), []float64{
+		0.5, -0.65, -0.3, 0.4,
+		0.55, 0.1, -0.32, -0.5,
+		0.25, 0.41, -0.21, 0.35,
+		0.06, 0.21, 0.0, -0.15,
+	}, 0.005) {
+		t.Error("x gradients don't match the expected values")
+	}
+}
+
+func TestConv2DStride2(t *testing.T) {
+
+	var g = ag.NewGraph()
+
+	x := g.NewVariable(mat.NewDense(4, 4, []float64{
+		0.2, 0.1, 0.5, 0.8,
+		0.4, -0.3, -0.2, -0.3,
+		0.5, -0.6, -0.4, 0.6,
+		-0.3, 0.9, 0.5, 0.5,
+	}), true)
+
+	w := g.NewVariable(mat.NewDense(2, 2, []float64{
+		0.5, -0.4,
+		0.3, 0.3,
+	}), true)
+
+	out := Conv2D(g, w, x, 2, 2)
+
+	if !floats.EqualApprox(out.Value().Data(), []float64{
+		0.09, -0.22,
+		0.67, -0.14,
+	}, 0.005) {
+		t.Error("out value doesn't match the expected values")
+	}
+
+	g.Backward(out, mat.NewDense(2, 2, []float64{
+		1.0, -0.5,
+		0.5, 0.3,
+	}))
+
+	if !floats.EqualApprox(w.Grad().Data(), []float64{
+		0.08, -0.42,
+		0.5, 0.45,
+	}, 0.005) {
+		t.Error("w gradients don't match the expected values")
+	}
+
+	if !floats.EqualApprox(x.Grad().Data(), []float64{
+		0.5, -0.4, -0.25, 0.2,
+		0.3, 0.3, -0.15, -0.15,
+		0.25, -0.2, 0.15, -0.12,
+		0.15, 0.15, 0.09, 0.09,
+	}, 0.005) {
+		t.Error("x gradients don't match the expected values")
+	}
+}
