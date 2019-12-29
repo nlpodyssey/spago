df23178f3ac7684ccbb3665e7baa0e99e5e30f7c
matteo-grella <matteogrella@gmail.com>
2019-12-29 01:17:03 +0100

    Implement the Perceptron

---
+++ pkg/ml/nn/perceptron/perceptron.go
@@ -0,0 +1,136 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package perceptron
+
+import (
+	"brillion.io/spago/pkg/mat"
+	"brillion.io/spago/pkg/ml/act"
+	"brillion.io/spago/pkg/ml/ag"
+	"brillion.io/spago/pkg/ml/nn"
+	"io"
+	"log"
+	"sync"
+)
+
+type Model struct {
+	W   *nn.Param    `type:"weights"`
+	B   *nn.Param    `type:"biases"`
+	Act act.FuncName // output activation
+}
+
+func New(in, out int, actFunc act.FuncName) *Model {
+	return &Model{
+		W:   nn.NewParam(mat.NewEmptyDense(out, in)),
+		B:   nn.NewParam(mat.NewEmptyVecDense(out)),
+		Act: actFunc,
+	}
+}
+
+func (m *Model) ForEachParam(callback func(param *nn.Param)) {
+	nn.ForEachParam(m, callback)
+}
+
+func (m *Model) Serialize(w io.Writer) (int, error) {
+	return nn.Serialize(m, w)
+}
+
+func (m *Model) Deserialize(r io.Reader) (int, error) {
+	return nn.Deserialize(m, r)
+}
+
+// SetActivation sets the new activation and returns the previous one.
+func (m *Model) SetActivation(a act.FuncName) act.FuncName {
+	prev := m.Act
+	m.Act = a
+	return prev
+}
+
+type Concurrency struct {
+	Value bool
+}
+
+type Processor struct {
+	opt         []interface{}
+	model       *Model
+	g           *ag.Graph
+	w           ag.Node
+	b           ag.Node
+	Concurrency bool
+}
+
+func (m *Model) NewProc(g *ag.Graph, opt ...interface{}) nn.Processor {
+	p := &Processor{
+		model:       m,
+		opt:         opt,
+		g:           g,
+		w:           g.NewWrap(m.W),
+		b:           g.NewWrap(m.B),
+		Concurrency: true,
+	}
+	p.init(opt)
+	return p
+}
+
+func (p *Processor) init(opt []interface{}) {
+	for _, t := range opt {
+		switch t := t.(type) {
+		case Concurrency:
+			p.Concurrency = t.Value
+		default:
+			log.Fatal("perceptron: invalid init options")
+		}
+	}
+}
+
+func (p *Processor) Model() nn.Model {
+	return p.model
+}
+
+func (p *Processor) Graph() *ag.Graph {
+	return p.g
+}
+
+func (p *Processor) RequiresFullSeq() bool {
+	return false
+}
+
+func (p *Processor) Reset() {
+	p.init(p.opt)
+}
+
+func (p *Processor) Forward(xs ...ag.Node) []ag.Node {
+	if p.Concurrency && len(xs) > 1 {
+		return p.fwdConcurrent(xs)
+	} else {
+		return p.fwdSerial(xs)
+	}
+}
+
+func (p *Processor) fwdSerial(xs []ag.Node) []ag.Node {
+	ys := make([]ag.Node, len(xs))
+	for i, x := range xs {
+		ys[i] = p.forward(x)
+	}
+	return ys
+}
+
+func (p *Processor) fwdConcurrent(xs []ag.Node) []ag.Node {
+	ys := make([]ag.Node, len(xs))
+	var wg sync.WaitGroup
+	wg.Add(len(xs))
+	for i := range xs {
+		go func(i int) {
+			defer wg.Done()
+			ys[i] = p.forward(xs[i])
+		}(i)
+	}
+	wg.Wait()
+	return ys
+}
+
+// y = f(w (dot) x + b)
+func (p *Processor) forward(x ag.Node) ag.Node {
+	return act.F(p.g, p.model.Act, nn.Affine(p.g, p.b, p.w, x))
+}
---
+++ pkg/ml/nn/perceptron/perceptron_test.go
@@ -0,0 +1,72 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package perceptron
+
+import (
+	"brillion.io/spago/pkg/mat"
+	"brillion.io/spago/pkg/ml/act"
+	"brillion.io/spago/pkg/ml/ag"
+	"brillion.io/spago/pkg/ml/losses"
+	"gonum.org/v1/gonum/floats"
+	"testing"
+)
+
+func TestModel_Forward(t *testing.T) {
+
+	model := newTestModel()
+	g := ag.NewGraph()
+
+	// == Forward
+
+	x := g.NewVariable(mat.NewVecDense([]float64{-0.8, -0.9, -0.9, 1.0}), true)
+	y := model.NewProc(g).Forward(x)[0]
+
+	if !floats.EqualApprox(y.Value().Data(), []float64{-0.39693, -0.79688, 0.0, 0.70137, -0.18775}, 1.0e-05) {
+		t.Error("The output doesn't match the expected values")
+	}
+
+	// == Backward
+
+	gold := g.NewVariable(mat.NewVecDense([]float64{0.0, 0.5, -0.4, -0.9, 0.9}), false)
+	loss := losses.MSE(g, y, gold, false)
+	g.Backward(loss)
+
+	if !floats.EqualApprox(x.Grad().Data(), []float64{0.0126, -2.07296, 1.07476, -0.14158}, 0.005) {
+		t.Error("The input gradients don't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.W.Grad().(*mat.Dense).Data(), []float64{
+		0.26751, 0.30095, 0.30095, -0.33439,
+		0.37867, 0.42601, 0.42601, -0.47334,
+		-0.32, -0.36, -0.36, 0.4,
+		-0.65089, -0.73226, -0.73226, 0.81362,
+		0.83952, 0.94446, 0.94446, -1.04940,
+	}, 1.0e-05) {
+		t.Error("W doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.B.Grad().Data(), []float64{
+		-0.33439, -0.47334, 0.4, 0.81362, -1.0494,
+	}, 1.0e-05) {
+		t.Error("B doesn't match the expected values")
+	}
+}
+
+func newTestModel() *Model {
+
+	model := New(4, 5, act.Tanh)
+
+	model.W.Value().SetData([]float64{
+		0.5, 0.6, -0.8, -0.6,
+		0.7, -0.4, 0.1, -0.8,
+		0.7, -0.7, 0.3, 0.5,
+		0.8, -0.9, 0.0, -0.1,
+		0.4, 1.0, -0.7, 0.8,
+	})
+
+	model.B.Value().SetData([]float64{0.4, 0.0, -0.3, 0.8, -0.4})
+
+	return model
+}
