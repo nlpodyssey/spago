ec55dd79a0052774dffad79c3374571a2956d8b6
matteo-grella <matteogrella@gmail.com>
2020-01-04 13:49:00 +0100

    Implement a generic bidirectional recurrent network (BiRNN)

---
+++ pkg/ml/nn/birnn/birnn.go
@@ -0,0 +1,136 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package birnn
+
+import (
+	"brillion.io/spago/pkg/ml/ag"
+	"brillion.io/spago/pkg/ml/nn"
+	"io"
+	"log"
+	"sync"
+)
+
+type MergeType int
+
+const (
+	Concat MergeType = iota // The outputs are concatenated together (the default)
+	Sum                     // The outputs are added together
+	Prod                    // The outputs are multiplied element-wise together
+	Avg                     // The average of the outputs is taken
+)
+
+type Model struct {
+	Positive  nn.Model // positive time direction a.k.a. left-to-right
+	Negative  nn.Model // negative time direction a.k.a. right-to-left
+	MergeMode MergeType
+}
+
+func New(positive, negative nn.Model, merge MergeType) *Model {
+	return &Model{
+		Positive:  positive,
+		Negative:  negative,
+		MergeMode: merge,
+	}
+}
+
+func (m *Model) ForEachParam(callback func(param *nn.Param)) {
+	nn.ForEachParam(m, callback)
+}
+
+func (m *Model) Serialize(w io.Writer) (int, error) {
+	return nn.Serialize(m, w)
+}
+
+func (m *Model) Deserialize(r io.Reader) (int, error) {
+	return nn.Deserialize(m, r)
+}
+
+type Processor struct {
+	opt      []interface{}
+	model    *Model
+	g        *ag.Graph
+	Positive nn.Processor
+	Negative nn.Processor
+}
+
+func (m *Model) NewProc(g *ag.Graph, opt ...interface{}) nn.Processor {
+	p := &Processor{
+		model:    m,
+		Positive: m.Positive.NewProc(g),
+		Negative: m.Negative.NewProc(g),
+		g:        g,
+	}
+	p.init(opt)
+	return p
+}
+
+func (p *Processor) init(opt []interface{}) {
+	if len(opt) > 0 {
+		log.Fatal("birnn: invalid init options")
+	}
+}
+
+func (p *Processor) Model() nn.Model {
+	return p.model
+}
+
+func (p *Processor) Graph() *ag.Graph {
+	return p.g
+}
+
+func (p *Processor) RequiresFullSeq() bool {
+	return true
+}
+
+func (p *Processor) Reset() {
+	p.Positive.Reset()
+	p.Positive.Reset()
+	p.init(p.opt)
+}
+
+func (p *Processor) Forward(xs ...ag.Node) []ag.Node {
+	var pos []ag.Node
+	var neg []ag.Node
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		pos = p.Positive.Forward(xs...)
+	}()
+	go func() {
+		defer wg.Done()
+		neg = p.Negative.Forward(reversed(xs)...)
+	}()
+	wg.Wait()
+	out := make([]ag.Node, len(pos))
+	for i := 0; i < len(xs); i++ {
+		out[i] = p.merge(pos[i], neg[len(out)-1-i])
+	}
+	return out
+}
+
+func reversed(ns []ag.Node) []ag.Node {
+	r := make([]ag.Node, len(ns))
+	copy(r, ns)
+	for i := 0; i < len(r)/2; i++ {
+		j := len(r) - i - 1
+		r[i], r[j] = r[j], r[i]
+	}
+	return r
+}
+
+func (p *Processor) merge(a, b ag.Node) ag.Node {
+	if p.model.MergeMode == Concat {
+		return p.g.Concat(a, b)
+	} else if p.model.MergeMode == Sum {
+		return p.g.Add(a, b)
+	} else if p.model.MergeMode == Prod {
+		return p.g.Prod(a, b)
+	} else if p.model.MergeMode == Avg {
+		return p.g.ProdScalar(p.g.Add(a, b), p.g.NewScalar(0.5))
+	} else {
+		panic("birnn: invalid merge mode")
+	}
+}
---
+++ pkg/ml/nn/birnn/birnn_test.go
@@ -0,0 +1,228 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package birnn
+
+import (
+	"brillion.io/spago/pkg/mat"
+	"brillion.io/spago/pkg/ml/ag"
+	"brillion.io/spago/pkg/ml/nn"
+	"brillion.io/spago/pkg/ml/nn/rec/srn"
+	"gonum.org/v1/gonum/floats"
+	"testing"
+)
+
+func TestModelConcat_Forward(t *testing.T) {
+	model := newTestModel(Concat)
+	g := ag.NewGraph()
+
+	// == Forward
+
+	x1 := g.NewVariable(mat.NewVecDense([]float64{0.5, 0.6}), true)
+	x2 := g.NewVariable(mat.NewVecDense([]float64{0.7, -0.4}), true)
+	x3 := g.NewVariable(mat.NewVecDense([]float64{0.0, -0.7}), true)
+
+	y := model.NewProc(g).Forward(x1, x2, x3)
+
+	if !floats.EqualApprox(y[0].Value().Data(), []float64{
+		0.187746, -0.50052, 0.109558, -0.005277, -0.084306, -0.628766,
+	}, 1.0e-06) {
+		t.Error("The first output doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(y[1].Value().Data(), []float64{
+		-0.704648, 0.200908, -0.064056, -0.329084, -0.237601, -0.449676,
+	}, 1.0e-06) {
+		t.Error("The first output doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(y[2].Value().Data(), []float64{
+		0.256521, 0.725227, 0.781582, 0.129273, -0.716298, -0.263625,
+	}, 1.0e-06) {
+		t.Error("The third output doesn't match the expected values")
+	}
+
+	// == Backward
+
+	y[0].PropagateGrad(mat.NewVecDense([]float64{-0.4, -0.8, 0.1, 0.4, 0.6, -0.4}))
+	y[1].PropagateGrad(mat.NewVecDense([]float64{0.6, 0.6, 0.7, 0.7, -0.6, 0.3}))
+	y[2].PropagateGrad(mat.NewVecDense([]float64{-0.1, -0.1, 0.1, -0.8, 0.4, -0.5}))
+
+	g.BackwardAll()
+
+	// Important! average params by sequence length
+	model.ForEachParam(func(param *nn.Param) {
+		param.Grad().ProdScalarInPlace(1.0 / 3.0)
+	})
+
+	if !floats.EqualApprox(x1.Grad().Data(), []float64{1.031472, -0.627913}, 0.006) {
+		t.Error("The first input gradients don't match the expected values")
+	}
+
+	if !floats.EqualApprox(x2.Grad().Data(), []float64{-0.539497, -0.629167}, 0.006) {
+		t.Error("The second input gradients don't match the expected values")
+	}
+
+	if !floats.EqualApprox(x3.Grad().Data(), []float64{0.013097, -0.09932}, 0.006) {
+		t.Error("The third input gradients don't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.Positive.(*srn.Model).W.Grad().(*mat.Dense).Data(), []float64{
+		0.001234, -0.107987,
+		0.175039, 0.015738,
+		0.213397, -0.046717,
+	}, 1.0e-06) {
+		t.Error("W-positive doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.Positive.(*srn.Model).WRec.Grad().(*mat.Dense).Data(), []float64{
+		0.041817, -0.059241, 0.013592,
+		0.042229, -0.086071, 0.019157,
+		0.035331, -0.11595, 0.02512,
+	}, 1.0e-06) {
+		t.Error("WRec-positive doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.Positive.(*srn.Model).B.Grad().Data(), []float64{
+		-0.071016, 0.268027, 0.345019,
+	}, 1.0e-06) {
+		t.Error("B-positive doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.Negative.(*srn.Model).W.Grad().(*mat.Dense).Data(), []float64{
+		0.145713, 0.234548,
+		0.050135, 0.070768,
+		-0.06125, -0.017281,
+	}, 1.0e-05) {
+		t.Error("W-negative doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.Negative.(*srn.Model).WRec.Grad().(*mat.Dense).Data(), []float64{
+		-0.029278, -0.112568, -0.089725,
+		-0.074426, 0.003116, -0.070784,
+		0.022664, 0.040583, 0.044139,
+	}, 1.0e-06) {
+		t.Error("WRec-negative doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.Negative.(*srn.Model).B.Grad().Data(), []float64{
+		-0.03906, 0.237598, -0.137858,
+	}, 1.0e-06) {
+		t.Error("B-negative doesn't match the expected values")
+	}
+}
+
+func TestModelSum_Forward(t *testing.T) {
+	model := newTestModel(Sum)
+	g := ag.NewGraph()
+
+	// == Forward
+
+	x1 := g.NewVariable(mat.NewVecDense([]float64{0.5, 0.6}), true)
+	x2 := g.NewVariable(mat.NewVecDense([]float64{0.7, -0.4}), true)
+	x3 := g.NewVariable(mat.NewVecDense([]float64{0.0, -0.7}), true)
+
+	y := model.NewProc(g).Forward(x1, x2, x3)
+
+	if !floats.EqualApprox(y[0].Value().Data(), []float64{0.182469, -0.584826, -0.519207}, 1.0e-06) {
+		t.Error("The first output doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(y[1].Value().Data(), []float64{-1.033731, -0.036692, -0.513732}, 1.0e-06) {
+		t.Error("The second output doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(y[2].Value().Data(), []float64{0.385793, 0.008929, 0.517957}, 1.0e-06) {
+		t.Error("The third output doesn't match the expected values")
+	}
+}
+
+func TestModelAvg_Forward(t *testing.T) {
+	model := newTestModel(Avg)
+	g := ag.NewGraph()
+
+	// == Forward
+
+	x1 := g.NewVariable(mat.NewVecDense([]float64{0.5, 0.6}), true)
+	x2 := g.NewVariable(mat.NewVecDense([]float64{0.7, -0.4}), true)
+	x3 := g.NewVariable(mat.NewVecDense([]float64{0.0, -0.7}), true)
+
+	y := model.NewProc(g).Forward(x1, x2, x3)
+
+	if !floats.EqualApprox(y[0].Value().Data(), []float64{0.0912345, -0.292413, -0.2596035}, 1.0e-06) {
+		t.Error("The first output doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(y[1].Value().Data(), []float64{-0.5168655, -0.018346, -0.256866}, 1.0e-06) {
+		t.Error("The second output doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(y[2].Value().Data(), []float64{0.1928965, 0.0044645, 0.2589785}, 1.0e-06) {
+		t.Error("The third output doesn't match the expected values")
+	}
+}
+
+func TestModelProd_Forward(t *testing.T) {
+	model := newTestModel(Prod)
+	g := ag.NewGraph()
+
+	// == Forward
+
+	x1 := g.NewVariable(mat.NewVecDense([]float64{0.5, 0.6}), true)
+	x2 := g.NewVariable(mat.NewVecDense([]float64{0.7, -0.4}), true)
+	x3 := g.NewVariable(mat.NewVecDense([]float64{0.0, -0.7}), true)
+
+	y := model.NewProc(g).Forward(x1, x2, x3)
+
+	if !floats.EqualApprox(y[0].Value().Data(), []float64{-0.00099, 0.042197, -0.068886}, 1.0e-06) {
+		t.Error("The first output doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(y[1].Value().Data(), []float64{0.231888, -0.047735, 0.028804}, 1.0e-06) {
+		t.Error("The second output doesn't match the expected values")
+	}
+
+	if !floats.EqualApprox(y[2].Value().Data(), []float64{0.033161, -0.519478, -0.206044}, 1.0e-06) {
+		t.Error("The third output doesn't match the expected values")
+	}
+}
+
+func newTestModel(mergeType MergeType) *Model {
+	model := New(
+		srn.New(2, 3),
+		srn.New(2, 3),
+		mergeType,
+	)
+	initPos(model.Positive.(*srn.Model))
+	initNeg(model.Negative.(*srn.Model))
+	return model
+}
+
+func initPos(m *srn.Model) {
+	m.W.Value().SetData([]float64{
+		-0.9, 0.4,
+		0.7, -1.0,
+		-0.9, -0.4,
+	})
+	m.WRec.Value().SetData([]float64{
+		0.1, 0.9, -0.5,
+		-0.6, 0.7, 0.7,
+		0.3, 0.9, 0.0,
+	})
+	m.B.Value().SetData([]float64{0.4, -0.3, 0.8})
+}
+
+func initNeg(m *srn.Model) {
+	m.W.Value().SetData([]float64{
+		0.3, 0.1,
+		0.6, 0.0,
+		-0.7, 0.1,
+	})
+	m.WRec.Value().SetData([]float64{
+		-0.2, 0.7, 0.7,
+		-0.2, 0.0, -1.0,
+		0.5, -0.4, 0.4,
+	})
+	m.B.Value().SetData([]float64{0.2, -0.9, -0.2})
+}
