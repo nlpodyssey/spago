a2f45e7b78a4b2ec62cdd82c54164d8507113288
matteo-grella <matteogrella@gmail.com>
2020-01-04 09:17:25 +0100

    Implement the squadred distance layer (SqrDist)

---
+++ pkg/ml/nn/sqrdist/sqrdist.go
@@ -0,0 +1,88 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sqrdist
+
+import (
+	"brillion.io/spago/pkg/mat"
+	"brillion.io/spago/pkg/ml/ag"
+	"brillion.io/spago/pkg/ml/nn"
+	"io"
+	"log"
+)
+
+type Model struct {
+	B *nn.Param `type:"weights"`
+}
+
+func New(in, rank int) *Model {
+	return &Model{
+		B: nn.NewParam(mat.NewEmptyDense(rank, in)),
+	}
+}
+
+func (m *Model) ForEachParam(callback func(param *nn.Param)) {
+	nn.ForEachParam(m, callback)
+}
+
+func (m *Model) Serialize(w io.Writer) (int, error) {
+	return nn.Serialize(m, w)
+}
+
+func (m *Model) Deserialize(r io.Reader) (int, error) {
+	return nn.Deserialize(m, r)
+}
+
+type Processor struct {
+	opt   []interface{}
+	model *Model
+	g     *ag.Graph
+	b     ag.Node
+}
+
+func (m *Model) NewProc(g *ag.Graph, opt ...interface{}) nn.Processor {
+	p := &Processor{
+		model: m,
+		opt:   opt,
+		g:     g,
+		b:     g.NewWrap(m.B),
+	}
+	p.init(opt)
+	return p
+}
+
+func (p *Processor) init(opt []interface{}) {
+	if len(opt) > 0 {
+		log.Fatal("sqrdist: invalid init options")
+	}
+}
+
+func (p *Processor) Model() nn.Model {
+	return p.model
+}
+
+func (p *Processor) Graph() *ag.Graph {
+	return p.g
+}
+
+func (p *Processor) RequiresFullSeq() bool {
+	return false
+}
+
+func (p *Processor) Reset() {
+	p.init(p.opt)
+}
+
+func (p *Processor) Forward(xs ...ag.Node) []ag.Node {
+	ys := make([]ag.Node, len(xs))
+	for i, x := range xs {
+		ys[i] = p.forward(x)
+	}
+	return ys
+}
+
+func (p *Processor) forward(x ag.Node) ag.Node {
+	bh := nn.Linear(p.g, p.b, x)
+	return nn.Linear(p.g, p.g.T(bh), bh)
+}
---
+++ pkg/ml/nn/sqrdist/sqrdist_test.go
@@ -0,0 +1,56 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sqrdist
+
+import (
+	"brillion.io/spago/pkg/mat"
+	"brillion.io/spago/pkg/ml/ag"
+	"gonum.org/v1/gonum/floats"
+	"testing"
+)
+
+func TestModel_Forward(t *testing.T) {
+	model := newTestModel()
+	g := ag.NewGraph()
+
+	// == Forward
+
+	x := g.NewVariable(mat.NewVecDense([]float64{0.3, 0.5, -0.4}), true)
+	y := model.NewProc(g).Forward(x)[0]
+
+	if !floats.EqualApprox(y.Value().Data(), []float64{0.5928}, 1.0e-05) {
+		t.Error("The output doesn't match the expected values")
+	}
+
+	// == Backward
+
+	g.Backward(y, mat.NewScalar(-0.8))
+
+	if !floats.EqualApprox(x.Grad().Data(), []float64{-0.9568, -0.848, 0.5936}, 1.0e-05) {
+		t.Error("The input gradients don't match the expected values")
+	}
+
+	if !floats.EqualApprox(model.B.Grad().(*mat.Dense).Data(), []float64{
+		-0.2976, -0.496, 0.3968,
+		0.0144, 0.024, -0.0192,
+		-0.1488, -0.248, 0.1984,
+		-0.1584, -0.264, 0.2112,
+		0.024, 0.04, -0.032,
+	}, 1.0e-06) {
+		t.Error("WIn doesn't match the expected values")
+	}
+}
+
+func newTestModel() *Model {
+	model := New(3, 5)
+	model.B.Value().SetData([]float64{
+		0.4, 0.6, -0.5,
+		-0.5, 0.4, 0.2,
+		0.5, 0.4, 0.1,
+		0.5, 0.2, -0.2,
+		-0.3, 0.4, 0.4,
+	})
+	return model
+}
