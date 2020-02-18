3d3828eea47d1fc4be7ce034bba2c262a9f9adf6
matteo-grella <matteogrella@gmail.com>
2020-02-18 12:24:14 +0100

    Add dense encoding

--- pkg/ml/fofe/encoder.go
+++ pkg/ml/fofe/encoder.go
@@ -9,6 +9,14 @@ import (
 	"saientist.dev/spago/pkg/utils"
 )
 
+func EncodeDense(alpha float64, size int, seq []int) []*mat.Dense {
+	y := make([]*mat.Dense, len(seq), len(seq))
+	for i, x := range Encode(alpha, size, seq) {
+		y[i] = mat.NewVecDense(x.Data())
+	}
+	return y
+}
+
 // The Fixed-Size Ordinally-Forgetting Encoding
 //    zt = α * zt−1 + et (1 ≤ t ≤ T)
 func Encode(alpha float64, size int, seq []int) []*mat.Sparse {
