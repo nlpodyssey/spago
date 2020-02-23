85cf6c84471cae607545d46d61d62714b3224c14
matteo-grella <matteogrella@gmail.com>
2020-02-23 05:16:18 +0100

    Move self-attention to the nn package

--- pkg/ml/nn/transforms.go
+++ pkg/ml/nn/transforms.go
@@ -5,7 +5,9 @@
 package nn
 
 import (
+	"saientist.dev/spago/pkg/mat"
 	"saientist.dev/spago/pkg/ml/ag"
+	"sync"
 )
 
 // Linear performs a linear transformation of the type Wx.
@@ -65,4 +67,44 @@ func Conv2D(g *ag.Graph, w, x ag.Node, xStride, yStride int) ag.Node {
 	}
 
 	return g.Reshape(g.Concat(outList...), dimx, dimy)
+}
+
+// ScaledDotProductAttention is a self-attention mechanism relating different positions of a single sequence in order to compute a representation of the same sequence.
+// This method requires that the query, the key and the value vectors have already been obtained from the input sequence.
+// The scaled factor is the square root of the dimension of the key vectors.
+func ScaledDotProductAttention(g *ag.Graph, qs, ks, vs []ag.Node, scaledFactor float64) (context []ag.Node, probs []mat.Matrix) {
+	context = make([]ag.Node, len(qs))
+	probs = make([]mat.Matrix, len(qs))
+	keys := g.Stack(ks...)
+	values := g.T(g.Stack(vs...))
+	divTerm := g.NewScalar(scaledFactor)
+	for i, q := range qs {
+		attScores := g.DivScalar(g.Mul(keys, q), divTerm)
+		attProbs := g.Softmax(attScores)
+		context[i] = g.Mul(values, attProbs)
+		probs[i] = attProbs.Value()
+	}
+	return
+}
+
+// ScaledDotProductAttentionConcurrent does the same thing as ScaledDotProductAttention but processes input concurrently.
+func ScaledDotProductAttentionConcurrent(g *ag.Graph, qs, ks, vs []ag.Node, scaledFactor float64) (context []ag.Node, probs []mat.Matrix) {
+	context = make([]ag.Node, len(qs))
+	probs = make([]mat.Matrix, len(qs))
+	keys := g.Stack(ks...)
+	values := g.T(g.Stack(vs...))
+	divTerm := g.NewScalar(scaledFactor)
+	var wg sync.WaitGroup
+	wg.Add(len(qs))
+	for i, q := range qs {
+		go func(i int, q ag.Node) {
+			defer wg.Done()
+			attScores := g.DivScalar(g.Mul(keys, q), divTerm)
+			attProbs := g.Softmax(attScores)
+			context[i] = g.Mul(values, attProbs)
+			probs[i] = attProbs.Value()
+		}(i, q)
+	}
+	wg.Wait()
+	return
 }
--- pkg/ml/nn/transforms_test.go
+++ pkg/ml/nn/transforms_test.go
@@ -6,6 +6,7 @@ package nn
 
 import (
 	"gonum.org/v1/gonum/floats"
+	"math"
 	"saientist.dev/spago/pkg/mat"
 	"saientist.dev/spago/pkg/ml/ag"
 	"testing"
@@ -103,5 +104,39 @@ func TestConv2DStride2(t *testing.T) {
 		0.15, 0.15, 0.09, 0.09,
 	}, 0.005) {
 		t.Error("x gradients don't match the expected values")
+	}
+}
+
+func TestScaledDotProductAttention(t *testing.T) {
+	g := ag.NewGraph()
+	qs := []ag.Node{
+		g.NewVariable(mat.NewVecDense([]float64{1.1, 0.0, 2.3}), true),
+		g.NewVariable(mat.NewVecDense([]float64{2.2, -0.5, 0.3}), true),
+		g.NewVariable(mat.NewVecDense([]float64{3.2, 0.5, 0.4}), true),
+	}
+	ks := []ag.Node{
+		g.NewVariable(mat.NewVecDense([]float64{0.0, 1.2, 1.3}), true),
+		g.NewVariable(mat.NewVecDense([]float64{4.5, 4.3, 0.2}), true),
+		g.NewVariable(mat.NewVecDense([]float64{2.7, 3.6, 2.1}), true),
+	}
+	vs := []ag.Node{
+		g.NewVariable(mat.NewVecDense([]float64{1.2, 2.3, 3.4}), true),
+		g.NewVariable(mat.NewVecDense([]float64{2.2, 8.5, 0.0}), true),
+		g.NewVariable(mat.NewVecDense([]float64{2.3, 6.5, 3.5}), true),
+	}
+
+	context, _ := ScaledDotProductAttention(g, qs, ks, vs, math.Sqrt(3))
+
+	if len(context) != 3 {
+		t.Error("The attention doesn't have the expected length")
+	}
+	if !floats.EqualApprox(context[0].Value().Data(), []float64{2.22875441063165, 6.68411289826994, 2.82497984315079}, 1.0e-6) {
+		t.Error("Attention[0] doesn't match the expected values")
+	}
+	if !floats.EqualApprox(context[1].Value().Data(), []float64{2.20637295180029, 8.15650999969648, 0.539678848469417}, 1.0e-6) {
+		t.Error("Attention[1] doesn't match the expected values")
+	}
+	if !floats.EqualApprox(context[2].Value().Data(), []float64{2.20423303670527, 8.41210390591632, 0.152898186332002}, 1.0e-6) {
+		t.Error("Attention[2] doesn't match the expected values")
 	}
 }
