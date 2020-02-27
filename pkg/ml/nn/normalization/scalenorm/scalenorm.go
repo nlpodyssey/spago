78582037d3e19b0f8079b7d85dd8fb0ed287315f
matteo-grella <matteogrella@gmail.com>
2020-02-27 14:27:07 +0100

    Implement ScaleNorm

---
+++ pkg/ml/nn/normalization/scalenorm/scalenorm.go
@@ -0,0 +1,78 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package scalenorm
+
+import (
+	"io"
+	"log"
+	"saientist.dev/spago/pkg/mat"
+	"saientist.dev/spago/pkg/ml/ag"
+	"saientist.dev/spago/pkg/ml/nn"
+)
+
+type Model struct {
+	W *nn.Param `type:"weights"`
+	B *nn.Param `type:"biases"`
+}
+
+func New(size int) *Model {
+	return &Model{
+		W: nn.NewParam(mat.NewEmptyVecDense(size)),
+		B: nn.NewParam(mat.NewEmptyVecDense(size)),
+	}
+}
+
+func (m *Model) ForEachParam(callback func(param *nn.Param)) {
+	nn.ForEachParam(m, callback)
+}
+
+func (m *Model) Serialize(w io.Writer) (int, error) {
+	return nn.Serialize(m, w)
+}
+
+func (m *Model) Deserialize(r io.Reader) (int, error) {
+	return nn.Deserialize(m, r)
+}
+
+type Processor struct {
+	opt   []interface{}
+	model *Model
+	g     *ag.Graph
+	w     ag.Node
+	b     ag.Node
+}
+
+func (m *Model) NewProc(g *ag.Graph, opt ...interface{}) nn.Processor {
+	p := &Processor{
+		model: m,
+		opt:   opt,
+		g:     g,
+		w:     g.NewWrap(m.W),
+		b:     g.NewWrap(m.B),
+	}
+	p.init(opt)
+	return p
+}
+
+func (p *Processor) init(opt []interface{}) {
+	if len(opt) > 0 {
+		log.Fatal("scalenorm: invalid init options")
+	}
+}
+
+func (p *Processor) Model() nn.Model       { return p.model }
+func (p *Processor) Graph() *ag.Graph      { return p.g }
+func (p *Processor) RequiresFullSeq() bool { return true }
+func (p *Processor) Reset()                { p.init(p.opt) }
+
+func (p *Processor) Forward(xs ...ag.Node) []ag.Node {
+	ys := make([]ag.Node, len(xs))
+	eps := p.g.NewScalar(1e-10)
+	for i, x := range xs {
+		norm := p.g.Sqrt(p.g.ReduceSum(p.g.Square(x)))
+		ys[i] = p.g.Add(p.g.Prod(p.g.DivScalar(x, p.g.AddScalar(norm, eps)), p.w), p.b)
+	}
+	return ys
+}
