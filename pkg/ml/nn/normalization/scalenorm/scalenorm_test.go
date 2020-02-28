c718e4094f7281509199a9a8be271767e123bb2e
matteo-grella <matteogrella@gmail.com>
2020-02-28 23:00:50 +0100

    Remove the biases from the test

--- pkg/ml/nn/normalization/scalenorm/scalenorm_test.go
+++ pkg/ml/nn/normalization/scalenorm/scalenorm_test.go
@@ -30,6 +30,5 @@ func TestModel_Forward(t *testing.T) {
 func newTestModel() *Model {
 	model := New(6)
 	model.Gain.Value().SetData([]float64{1.0, 1.0, 1.0, 1.0, 1.0, 1.0})
-	model.B.Value().SetData([]float64{0.0, 0.0, 0.0, 0.0, 0.0, 0.0})
 	return model
 }
