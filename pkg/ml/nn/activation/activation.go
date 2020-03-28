f046bdfd4f4ff19e8548a866de878b5530ebe11d
matteo-grella <matteogrella@gmail.com>
2020-03-29 00:36:22 +0100

    First attempt to use the activation functions as if they were a neural model (e.g. to be inserted in a stack of layers) Proceeding in this direction would allow to replace the not very solid use of the activation functions in models such as the 'perceptron'

---
+++ pkg/ml/nn/activation/activation.go
@@ -0,0 +1,80 @@
+// Copyright 2020 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package activation
+
+import (
+	"github.com/nlpodyssey/spago/pkg/ml/ag"
+	"github.com/nlpodyssey/spago/pkg/ml/nn"
+	"io"
+	"log"
+)
+
+var _ nn.Model = &Model{}
+
+type Model struct {
+	Activation ag.OpName
+	Params     []*nn.Param
+}
+
+// TODO: restrict operators to activation functions only; or create a dedicate builder for each activation.
+func New(activation ag.OpName, params ...*nn.Param) *Model {
+	return &Model{
+		Activation: activation,
+		Params:     params,
+	}
+}
+
+func (m *Model) ForEachParam(callback func(param *nn.Param)) {
+	nn.ForEachParam(m, callback)
+}
+
+func (m *Model) Serialize(w io.Writer) (int, error) {
+	return nn.Serialize(m, w)
+}
+
+func (m *Model) Deserialize(r io.Reader) (int, error) {
+	return nn.Deserialize(m, r)
+}
+
+type Processor struct {
+	opt    []interface{}
+	model  *Model
+	g      *ag.Graph
+	params []ag.Node
+}
+
+func (m *Model) NewProc(g *ag.Graph, opt ...interface{}) nn.Processor {
+	var params []ag.Node
+	for _, param := range m.Params {
+		params = append(params, g.NewWrap(param))
+	}
+	p := &Processor{
+		model:  m,
+		opt:    opt,
+		g:      g,
+		params: params,
+	}
+	p.init(opt)
+	return p
+}
+
+func (p *Processor) init(opt []interface{}) {
+	if len(opt) > 0 {
+		log.Fatal("activation: invalid init options")
+	}
+}
+
+func (p *Processor) Model() nn.Model       { return p.model }
+func (p *Processor) Graph() *ag.Graph      { return p.g }
+func (p *Processor) RequiresFullSeq() bool { return false }
+func (p *Processor) Reset()                { p.init(p.opt) }
+
+func (p *Processor) Forward(xs ...ag.Node) []ag.Node {
+	ys := make([]ag.Node, len(xs))
+	for i, x := range xs {
+		ys[i] = p.g.Invoke(p.model.Activation, append([]ag.Node{x}, p.params...)...)
+	}
+	return ys
+}
---
+++ pkg/ml/nn/activation/activation_test.go
@@ -0,0 +1,58 @@
+// Copyright 2020 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package activation
+
+import (
+	"github.com/nlpodyssey/spago/pkg/mat"
+	"github.com/nlpodyssey/spago/pkg/ml/ag"
+	"github.com/nlpodyssey/spago/pkg/ml/nn"
+	"gonum.org/v1/gonum/floats"
+	"testing"
+)
+
+func TestModelReLU_Forward(t *testing.T) {
+	g := ag.NewGraph()
+
+	// == Forward
+	x := g.NewVariable(mat.NewVecDense([]float64{0.1, -0.2, 0.3, 0.0}), true)
+	y := New(ag.ReLU).NewProc(g).Forward(x)[0]
+
+	if !floats.EqualApprox(y.Value().Data(), []float64{0.1, 0.0, 0.3, 0.0}, 1.0e-05) {
+		t.Error("The output doesn't match the expected values")
+	}
+
+	// == Backward
+	g.Backward(y, mat.NewVecDense([]float64{-1.0, 0.5, 0.8, 0.0}))
+
+	if !floats.EqualApprox(x.Grad().Data(), []float64{-1.0, 0.0, 0.8, 0.0}, 1.0e-6) {
+		t.Error("The x-gradients don't match the expected values")
+	}
+}
+
+func TestModelSwish_Forward(t *testing.T) {
+	g := ag.NewGraph()
+
+	beta := nn.NewParam(mat.NewScalar(2.0))
+	model := New(ag.Swish, beta)
+
+	// == Forward
+	x := g.NewVariable(mat.NewVecDense([]float64{0.1, -0.2, 0.3, 0.0}), true)
+	y := model.NewProc(g).Forward(x)[0]
+
+	if !floats.EqualApprox(y.Value().Data(), []float64{0.0549833997, -0.080262468, 0.1936968919, 0.0}, 1.0e-6) {
+		t.Error("The output doesn't match the expected values")
+	}
+
+	// == Backward
+	g.Backward(y, mat.NewVecDense([]float64{-1.0, 0.5, 0.8, 0.0}))
+
+	if !floats.EqualApprox(x.Grad().Data(), []float64{-0.5993373119, 0.1526040208, 0.6263414804, 0.0}, 1.0e-6) {
+		t.Error("The x-gradients don't match the expected values")
+	}
+
+	if !floats.EqualApprox(beta.Grad().Data(), []float64{0.0188025145}, 1.0e-6) {
+		t.Error("The beta-gradients don't match the expected values")
+	}
+}
