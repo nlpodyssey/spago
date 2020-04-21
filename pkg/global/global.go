7e96e2d38c90c0c6e3613c889d3ddc608b9f3ad1
matteo-grella <matteogrella@gmail.com>
2020-04-21 23:08:41 +0200

    Sketch a way to set a global level of optimization; TODO: surely it needs to be replaced with a more robust and standardized approach

---
+++ pkg/global/global.go
@@ -0,0 +1,49 @@
+// Copyright 2020 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package global
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// This is used to regulate the use of approximate math functions over the default implementations.
+var mathOptimizationLevel = 0
+
+const minMathOptimizationLevel = 0
+const maxMathOptimizationLevel = 2
+const defaultMathOptimizationLevel = 0
+
+func init() {
+	strOptLevel := os.Getenv("OPTIMIZATION_LEVEL")
+	if strOptLevel == "" {
+		SetMathOptimizationLevel(defaultMathOptimizationLevel)
+	} else {
+		if i, err := strconv.Atoi(strOptLevel); err == nil {
+			SetMathOptimizationLevel(i)
+		} else {
+			panic(fmt.Sprintf("global: optimization level must be a number in the range [%d-%d]",
+				minMathOptimizationLevel, maxMathOptimizationLevel))
+		}
+	}
+}
+
+// SetMathOptimizationLevel the global optimization level to i.
+// It returns the previous level.
+func SetMathOptimizationLevel(i int) int {
+	if !(i >= minMathOptimizationLevel && i <= maxMathOptimizationLevel) {
+		panic(fmt.Sprintf("global: optimization level must be in the range [%d-%d], found %d",
+			minMathOptimizationLevel, maxMathOptimizationLevel, i))
+	}
+	prev := mathOptimizationLevel
+	mathOptimizationLevel = i
+	return prev
+}
+
+// MathOptimizationLevel returns the global optimization
+func MathOptimizationLevel() int {
+	return mathOptimizationLevel
+}
