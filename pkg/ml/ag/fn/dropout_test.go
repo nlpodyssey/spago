5c13203b0f396cbcf7608414653b86f9a241c7ac
matteo-grella <matteogrella@gmail.com>
2020-04-30 15:22:56 +0200

    Update dropout tests

--- pkg/ml/ag/fn/dropout_test.go
+++ pkg/ml/ag/fn/dropout_test.go
@@ -12,7 +12,6 @@ import (
 )
 
 func TestDropout_Forward(t *testing.T) {
-
 	x := &variable{
 		value:        mat.NewVecDense([]float64{0.5, 0.6, -0.8, -0.6, 0.7, -0.4, 0.1, -0.8, 0.3, -0.5}),
 		grad:         nil,
@@ -22,16 +21,21 @@ func TestDropout_Forward(t *testing.T) {
 	y := f.Forward()
 
 	if !floats.EqualApprox(y.Data(), []float64{
-		0.666666, 0.799999, -1.066666, -0.799999, 0.0, -0.5333333, 0.133333, 0.0, 0.399999, -0.666666, // TODO: check values
+		0.666666, 0.799999, -1.066666, -0.799999, 0.0, -0.5333333, 0.133333, 0.0, 0.399999, -0.666666,
 	}, 1.0e-6) {
 		t.Error("The output doesn't match the expected values")
 	}
 
-	// TODO: check backward
+	f.Backward(mat.NewVecDense([]float64{0.5, 0.4, 0.2, -0.6, 0.3, 0.4, -0.8, -0.3, 0.0, 0.1}))
+
+	if !floats.EqualApprox(x.grad.Data(), []float64{
+		0.666666666, 0.533333333, 0.2666666, -0.8, 0.0, 0.5333333, -1.0666666, 0.0, 0.0, 0.133333,
+	}, 1.0e-6) {
+		t.Error("The x-gradients don't match the expected values")
+	}
 }
 
 func TestZeroDropout_Forward(t *testing.T) {
-
 	x := &variable{
 		value:        mat.NewVecDense([]float64{0.5, 0.6, -0.8, -0.6, 0.7, -0.4, 0.1, -0.8, 0.3, -0.5}),
 		grad:         nil,
@@ -41,16 +45,21 @@ func TestZeroDropout_Forward(t *testing.T) {
 	y := f.Forward()
 
 	if !floats.EqualApprox(y.Data(), []float64{
-		0.5, 0.6, -0.8, -0.6, 0.7, -0.4, 0.1, -0.8, 0.3, -0.5, // TODO: check values
+		0.5, 0.6, -0.8, -0.6, 0.7, -0.4, 0.1, -0.8, 0.3, -0.5,
 	}, 1.0e-6) {
 		t.Error("The output don't match the expected values")
 	}
 
-	// TODO: check backward
+	f.Backward(mat.NewVecDense([]float64{0.5, 0.4, 0.2, -0.6, 0.3, 0.4, -0.8, -0.3, 0.0, 0.1}))
+
+	if !floats.EqualApprox(x.grad.Data(), []float64{
+		0.5, 0.4, 0.2, -0.6, 0.3, 0.4, -0.8, -0.3, 0.0, 0.1,
+	}, 1.0e-6) {
+		t.Error("The x-gradients don't match the expected values")
+	}
 }
 
 func TestTotalDropout_Forward(t *testing.T) {
-
 	x := &variable{
 		value:        mat.NewVecDense([]float64{0.5, 0.6, -0.8, -0.6, 0.7, -0.4, 0.1, -0.8, 0.3, -0.5}),
 		grad:         nil,
@@ -60,10 +69,16 @@ func TestTotalDropout_Forward(t *testing.T) {
 	y := f.Forward()
 
 	if !floats.EqualApprox(y.Data(), []float64{
-		0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, // TODO: check values
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 	}, 1.0e-6) {
 		t.Error("The output don't match the expected values")
 	}
 
-	// TODO: check backward
+	f.Backward(mat.NewVecDense([]float64{0.5, 0.4, 0.2, -0.6, 0.3, 0.4, -0.8, -0.3, 0.0, 0.1}))
+
+	if !floats.EqualApprox(x.grad.Data(), []float64{
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+	}, 1.0e-6) {
+		t.Error("The x-gradients don't match the expected values")
+	}
 }
