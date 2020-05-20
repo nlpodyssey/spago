0f23fe51503f95b3ce831e8085434834e19acb61
matteo-grella <matteogrella@gmail.com>
2020-05-20 22:47:25 +0200

    Sample the next character from a probability distribution

--- pkg/nlp/charlm/utils.go
+++ pkg/nlp/charlm/utils.go
@@ -4,10 +4,32 @@
 
 package charlm
 
+import (
+	"github.com/nlpodyssey/spago/pkg/mat/f64utils"
+	"golang.org/x/exp/rand"
+)
+
 func splitByRune(str string) []string {
 	out := make([]string, 0)
 	for _, item := range str {
 		out = append(out, string(item))
 	}
 	return out
+}
+
+// sample extracts the next character from the probability multinomial distribution.
+// Note that the softmax must NOT have been applied to the prediction values.
+func sample(prediction []float64, temperature float64) int {
+	for i := range prediction {
+		prediction[i] *= 1.0 / temperature
+	}
+	prediction = f64utils.SoftMax(prediction)
+	p := rand.Float64() // TODO: use a local random generator?
+	for i, x := range prediction {
+		p -= x
+		if p < 0 {
+			return i
+		}
+	}
+	return 0 // TODO: should panic here?
 }
