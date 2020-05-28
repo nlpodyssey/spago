7cc637824c3020cb4e944d4dc91989a3b6ea3ec7
matteo-grella <matteogrella@gmail.com>
2020-05-28 15:47:26 +0200

    Add `ZeroEmbedding`

--- pkg/nlp/embeddings/embeddings.go
+++ pkg/nlp/embeddings/embeddings.go
@@ -33,17 +33,21 @@ type Model struct {
 	storage        kvdb.KeyValueDB
 	mu             sync.Mutex
 	UsedEmbeddings map[string]*nn.Param `type:"weights"`
+	ZeroEmbedding  *nn.Param            `type:"weights"`
 }
 
 // TODO: add Dropout
 type Config struct {
-	// Size of the embedding vectors
+	// Size of the embedding vectors.
 	Size int
+	// Whether to return the `ZeroEmbedding` in case the word doesn't exist in the embeddings map.
+	// If it is false, nil is returned instead, so the caller has more responsibility but also more control.
+	UseZeroEmbedding bool
 	// The path to DB on the drive
 	DBPath string
 	// Whether to use the map in read-only mode (embeddings are not updated during training).
 	ReadOnly bool
-	// Whether to force the deletion of any existing DB to start with an empty embeddings map
+	// Whether to force the deletion of any existing DB to start with an empty embeddings map.
 	ForceNewDB bool
 }
 
@@ -57,7 +61,9 @@ func New(config Config) *Model {
 			ForceNew: config.ForceNewDB,
 		}),
 		UsedEmbeddings: map[string]*nn.Param{},
+		ZeroEmbedding:  nn.NewParam(mat.NewEmptyVecDense(config.Size)),
 	}
+	nn.RequiresGrad(false)(m.ZeroEmbedding)
 	allModels = append(allModels, m)
 	return m
 }
@@ -144,7 +150,7 @@ func (m *Model) GetEmbedding(word string) *nn.Param {
 // The returned embedding is also cached in m.UsedEmbeddings for two reasons:
 //     - to allow a faster recovery;
 //     - to keep track of used embeddings, should they be optimized.
-// If no embedding is found, nil is returned.
+// If no embedding is found, nil or the `ZeroEmbedding` is returned, depending on the configuration.
 // It panics in case of storage errors.
 func (m *Model) getEmbedding(word string) *nn.Param {
 	if embedding, ok := m.UsedEmbeddings[word]; ok {
@@ -217,11 +223,14 @@ func (m *Model) Load(filename string) {
 
 type Processor struct {
 	nn.BaseProcessor
-	UsedEmbeddings map[string]*nn.Param
-	getEmbedding   func(word string) *nn.Param
+	zeroEmbedding ag.Node
 }
 
 func (m *Model) NewProc(g *ag.Graph) nn.Processor {
+	var zeroEmbedding ag.Node = nil
+	if m.UseZeroEmbedding {
+		zeroEmbedding = g.NewWrap(m.ZeroEmbedding)
+	}
 	return &Processor{
 		BaseProcessor: nn.BaseProcessor{
 			Model:             m,
@@ -229,7 +238,7 @@ func (m *Model) NewProc(g *ag.Graph) nn.Processor {
 			Graph:             g,
 			FullSeqProcessing: false,
 		},
-		getEmbedding: m.GetEmbedding,
+		zeroEmbedding: zeroEmbedding, // it can be nil
 	}
 }
 
@@ -244,15 +253,20 @@ func (p *Processor) Encode(words []string) []ag.Node {
 			encoding[i] = item
 		} else {
 			embedding := p.getEmbedding(word)
-			if embedding != nil {
-				encoding[i] = p.Graph.NewWrap(embedding)
-			} else {
-				encoding[i] = nil
-			}
-			cache[word] = encoding[i]
+			encoding[i], cache[word] = embedding, embedding
 		}
 	}
 	return encoding
+}
+
+func (p *Processor) getEmbedding(words string) ag.Node {
+	model := p.Model.(*Model)
+	switch param := model.GetEmbedding(words); {
+	case param == nil:
+		return p.zeroEmbedding // it can be nil
+	default:
+		return p.Graph.NewWrap(param)
+	}
 }
 
 func (p *Processor) Forward(_ ...ag.Node) []ag.Node {
