eeb396e4e6c53f6d69f86791903e0aff9a8d200a
matteo-grella <matteogrella@gmail.com>
2020-05-30 02:19:01 +0200

    Automate the download of pre-defined models; TODO: It needs refactoring badly

--- cmd/ner/main.go
+++ cmd/ner/main.go
@@ -7,8 +7,11 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
+	"github.com/dustin/go-humanize"
 	"github.com/nlpodyssey/spago/pkg/ml/nn"
 	"github.com/nlpodyssey/spago/pkg/ml/nn/birnn"
 	"github.com/nlpodyssey/spago/pkg/ml/nn/birnncrf"
@@ -22,25 +25,60 @@ import (
 	"github.com/nlpodyssey/spago/pkg/nlp/stackedembeddings"
 	"github.com/nlpodyssey/spago/pkg/nlp/vocabulary"
 	"github.com/nlpodyssey/spago/pkg/utils"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 )
 
+var predefinedModels = map[string]string{
+	"goflair-en-ner-conll03": "https://dl.dropboxusercontent.com/s/jgyv568v0nd4ogx/goflair-en-ner-conll03.tar.gz?dl=0",
+}
+
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage:", os.Args[0], "port", "path/to/config.json")
+	if len(os.Args) != 4 {
+		fmt.Println("Usage:", os.Args[0], "port", "path/to/models/", "model-name")
 		return
 	}
+
 	port, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	configPath := os.Args[2]
+	modelsFolder := os.Args[2]
+	if _, err := os.Stat(modelsFolder); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	modelName := os.Args[3]
+	modelPath := filepath.Join(modelsFolder, modelName)
+	if _, err := os.Stat(modelPath); os.IsNotExist(err) {
+		switch url, ok := predefinedModels[modelName]; {
+		case ok:
+			fmt.Printf("Fetch model from `%s`\n", url)
+			if err := DownloadFile(fmt.Sprintf("%s-compressed", modelPath), url); err != nil {
+				log.Fatal(err)
+			}
+			r, err := os.Open(fmt.Sprintf("%s-compressed", modelPath))
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Print("Extracting compressed model... ")
+			ExtractTarGz(r, modelsFolder)
+			fmt.Println("ok")
+		default:
+			log.Fatal(err)
+		}
+	}
+
+	configPath := filepath.Join(modelPath, "config.json")
 	config := loadConfig(configPath)
-	model := buildNewDefaultModel(config)
-	loadModelParams(config.ModelPath, model)
+	model := buildNewDefaultModel(config, modelPath)
+	loadModelParams(filepath.Join(modelPath, config.ModelFilename), model)
 
 	fmt.Println(fmt.Sprintf("Start server on port %d.", port))
 	server := sequencelabeler.NewServer(model, port)
@@ -48,7 +86,7 @@ func main() {
 }
 
 type Config struct {
-	ModelPath                      string                     `json:"model_path"`
+	ModelFilename                  string                     `json:"model_filename"`
 	WordEmbeddings                 WordEmbeddingsConfig       `json:"word_embeddings"`
 	ContextualStringEmbeddings     ContextualEmbeddingsConfig `json:"contextual_string_embeddings"`
 	EmbeddingsProjectionInputSize  int                        `json:"embeddings_projection_input_size"`
@@ -61,17 +99,17 @@ type Config struct {
 }
 
 type ContextualEmbeddingsConfig struct {
-	VocabularySize    int    `json:"vocabulary_size"`
-	EmbeddingSize     int    `json:"embedding_size"`
-	HiddenSize        int    `json:"hidden_size"`
-	SequenceSeparator string `json:"sequence_separator"`
-	UnknownToken      string `json:"unknown_token"`
-	VocabularyPath    string `json:"vocabulary_path"`
+	VocabularySize     int    `json:"vocabulary_size"`
+	EmbeddingSize      int    `json:"embedding_size"`
+	HiddenSize         int    `json:"hidden_size"`
+	SequenceSeparator  string `json:"sequence_separator"`
+	UnknownToken       string `json:"unknown_token"`
+	VocabularyFilename string `json:"vocabulary_filename"`
 }
 
 type WordEmbeddingsConfig struct {
-	WordEmbeddingsPath string `json:"embeddings_path"`
-	WordEmbeddingsSize int    `json:"embeddings_size"`
+	WordEmbeddingsFilename string `json:"embeddings_filename"`
+	WordEmbeddingsSize     int    `json:"embeddings_size"`
 }
 
 func loadConfig(file string) Config {
@@ -90,7 +128,7 @@ func loadConfig(file string) Config {
 
 // buildNewDefaultModel returns a new sequence labeler built based on the architecture of Flair.
 // See https://github.com/flairNLP/flair for more information.
-func buildNewDefaultModel(config Config) *sequencelabeler.Model {
+func buildNewDefaultModel(config Config, path string) *sequencelabeler.Model {
 	CharLanguageModelConfig := charlm.Config{
 		VocabularySize:    config.ContextualStringEmbeddings.VocabularySize,
 		EmbeddingSize:     config.ContextualStringEmbeddings.EmbeddingSize,
@@ -105,7 +143,7 @@ func buildNewDefaultModel(config Config) *sequencelabeler.Model {
 				embeddings.New(embeddings.Config{
 					Size:             config.WordEmbeddings.WordEmbeddingsSize,
 					UseZeroEmbedding: true,
-					DBPath:           config.WordEmbeddings.WordEmbeddingsPath,
+					DBPath:           filepath.Join(path, config.WordEmbeddings.WordEmbeddingsFilename),
 					ReadOnly:         true,
 					ForceNewDB:       false,
 				}),
@@ -131,7 +169,7 @@ func buildNewDefaultModel(config Config) *sequencelabeler.Model {
 		Labels: config.Labels,
 	}
 
-	vocab := loadVocabulary(config.ContextualStringEmbeddings.VocabularyPath)
+	vocab := loadVocabulary(filepath.Join(path, config.ContextualStringEmbeddings.VocabularyFilename))
 	l2rCharLM := m.EmbeddingsLayer.WordsEncoders[1].(*contextualstringembeddings.Model).LeftToRight
 	r2lCharLM := m.EmbeddingsLayer.WordsEncoders[1].(*contextualstringembeddings.Model).RightToLeft
 	l2rCharLM.Vocabulary, r2lCharLM.Vocabulary = vocab, vocab
@@ -159,4 +197,113 @@ func loadModelParams(file string, model *sequencelabeler.Model) {
 		panic("error during model deserialization.")
 	}
 	fmt.Println("ok")
+}
+
+// ====
+// What follows is a copy-and-paste from https://golangcode.com/download-a-file-with-progress/
+// ====
+
+// WriteCounter counts the number of bytes written to it. It implements to the io.Writer interface
+// and we can pass this into io.TeeReader() which will report progress on each write cycle.
+type WriteCounter struct {
+	Total uint64
+}
+
+func (wc *WriteCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+	return n, nil
+}
+
+func (wc WriteCounter) PrintProgress() {
+	// Clear the line by using a character return to go back to the start and remove
+	// the remaining characters by filling it with spaces
+	fmt.Printf("\r%s", strings.Repeat(" ", 35))
+
+	// Return again and print current status of download
+	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
+	fmt.Printf("\rDownloading... %s complete", humanize.Bytes(wc.Total))
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory. We pass an io.TeeReader
+// into Copy() to report progress on the download.
+func DownloadFile(filepath string, url string) error {
+
+	// Create the file, but give it a tmp file extension, this means we won't overwrite a
+	// file until it's downloaded, but we'll remove the tmp extension once downloaded.
+	out, err := os.Create(filepath + ".tmp")
+	if err != nil {
+		return err
+	}
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		out.Close()
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create our progress reporter and pass it to be used alongside our writer
+	counter := &WriteCounter{}
+	if _, err = io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
+		out.Close()
+		return err
+	}
+
+	// The progress use the same line so print a new line once it's finished downloading
+	fmt.Print("\n")
+
+	// Close the file without defer so it can happen before Rename()
+	out.Close()
+
+	if err = os.Rename(filepath+".tmp", filepath); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ====
+// What follows is a copy-and-paste from https://stackoverflow.com/questions/57639648/how-to-decompress-tar-gz-file-in-go
+// ====
+
+func ExtractTarGz(gzipStream io.Reader, path string) {
+	uncompressedStream, err := gzip.NewReader(gzipStream)
+	if err != nil {
+		log.Fatal("ExtractTarGz: NewReader failed")
+	}
+	tarReader := tar.NewReader(uncompressedStream)
+
+	for true {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("ExtractTarGz: Next() failed: %s", err.Error())
+		}
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.Mkdir(filepath.Join(path, header.Name), 0755); err != nil {
+				log.Fatalf("ExtractTarGz: Mkdir() failed: %s", err.Error())
+			}
+		case tar.TypeReg:
+			outFile, err := os.Create(filepath.Join(path, header.Name))
+			if err != nil {
+				log.Fatalf("ExtractTarGz: Create() failed: %s", err.Error())
+			}
+			if _, err := io.Copy(outFile, tarReader); err != nil {
+				log.Fatalf("ExtractTarGz: Copy() failed: %s", err.Error())
+			}
+			outFile.Close()
+
+		default:
+			log.Fatalf(
+				"ExtractTarGz: uknown type: %s in %s",
+				header.Typeflag,
+				header.Name)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,10 @@ go 1.14
 
 require (
 	github.com/dgraph-io/badger/v2 v2.0.3
-	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
-	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
 	github.com/gosuri/uilive v0.0.4 // indirect
 	github.com/gosuri/uiprogress v0.0.1
-	github.com/james-bowman/sparse v0.0.0-20191203082507-0ce573ca6b71
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/nlpodyssey/gopickle v0.0.0-20200517121026-fe7413eee34e
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/exp v0.0.0-20191227195350-da58074b4299
 	golang.org/x/text v0.3.0
