349e65d60748e571396bbb3e008f62c5b26e8fa8
matteo-grella <matteogrella@gmail.com>
2020-06-20 01:05:36 +0200

    Remove implicit type

--- pkg/ml/encoding/fofe/decoder_test.go
+++ pkg/ml/encoding/fofe/decoder_test.go
@@ -13,10 +13,10 @@ func TestDecode(t *testing.T) {
 	reverseVocabulary := map[int]string{0: "a", 1: "b", 2: "c", 3: "d", 4: "e"}
 
 	z := mat.NewSparseFromMap(len(reverseVocabulary), 1, map[mat.Coordinate]float64{
-		mat.Coordinate{I: 0, J: 0}: 1.00781250,
-		mat.Coordinate{I: 2, J: 0}: 0.51562500,
-		mat.Coordinate{I: 3, J: 0}: 0.28125000,
-		mat.Coordinate{I: 4, J: 0}: 0.18750000,
+		{I: 0, J: 0}: 1.00781250,
+		{I: 2, J: 0}: 0.51562500,
+		{I: 3, J: 0}: 0.28125000,
+		{I: 4, J: 0}: 0.18750000,
 	})
 
 	decoding := Decode(0.5, z)
