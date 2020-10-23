2744dd729c92021c522cd4fa42428fbd63e6736a
Juliano Viana <juliano.viana@kirasystems.com>
2020-10-23 15:41:55 -0400

    fixing implementation of batchnorm, adding support for inference

--- pkg/ml/nn/normalization/batchnorm/batchnorm_test.go
+++ pkg/ml/nn/normalization/batchnorm/batchnorm_test.go
@@ -5,12 +5,170 @@
 package batchnorm
 
 import (
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"testing"
+
 	"github.com/nlpodyssey/spago/pkg/mat"
 	"github.com/nlpodyssey/spago/pkg/ml/ag"
+	"github.com/nlpodyssey/spago/pkg/ml/nn"
+	"github.com/nlpodyssey/spago/pkg/utils"
+	"github.com/stretchr/testify/require"
 	"gonum.org/v1/gonum/floats"
-	"testing"
 )
 
+func TestModel_Forward_Params(t *testing.T) {
+	const numDataInstances = 10000
+	const dataSize = 100
+
+	testCases := []struct {
+		shift          float64
+		multiplier     float64
+		momentum       float64
+		expectedAvg    float64
+		expectedStdDev float64
+		forwardSteps   int
+	}{
+		{
+			multiplier:     1.0,
+			momentum:       0.0,
+			expectedAvg:    0.0,
+			expectedStdDev: 1.0,
+			forwardSteps:   1,
+		},
+		{
+			shift:          10.0,
+			multiplier:     1.0,
+			momentum:       0.0,
+			expectedAvg:    10.0,
+			expectedStdDev: 1.0,
+			forwardSteps:   1,
+		},
+		{
+			multiplier:     1.0,
+			momentum:       0.5,
+			expectedAvg:    0.0,
+			expectedStdDev: 0.5,
+			forwardSteps:   1,
+		},
+		{
+			multiplier:     1.0,
+			momentum:       1.0,
+			expectedAvg:    0.0,
+			expectedStdDev: 0.0,
+			forwardSteps:   1,
+		},
+		{
+			multiplier:     2.0,
+			momentum:       0.0,
+			expectedAvg:    0.0,
+			expectedStdDev: 2.0,
+			forwardSteps:   1,
+		},
+		{
+			multiplier:     1.0,
+			momentum:       0.5,
+			expectedAvg:    0.0,
+			expectedStdDev: 0.5,
+			forwardSteps:   1,
+		},
+		{
+			multiplier:     1.0,
+			momentum:       0.5,
+			expectedAvg:    0.0,
+			expectedStdDev: 0.75,
+			forwardSteps:   2,
+		},
+	}
+
+	rnd := rand.New(rand.NewSource(42))
+
+	testData := make([][]float64, numDataInstances)
+	for i := range testData {
+		testData[i] = make([]float64, dataSize)
+		for j := range testData[i] {
+			testData[i][j] = rnd.NormFloat64()
+		}
+	}
+
+	for _, tt := range testCases {
+
+		g := ag.NewGraph()
+		model := NewWithMomentum(dataSize, tt.momentum)
+		data := make([][]float64, len(testData))
+		for i := range testData {
+			data[i] = make([]float64, dataSize)
+			for j := range testData[i] {
+				data[i][j] = tt.multiplier*testData[i][j] + tt.shift
+			}
+		}
+		x := make([]ag.Node, len(testData))
+		for i := range x {
+			x[i] = g.NewVariable(mat.NewVecDense(data[i]), false)
+		}
+
+		var y []ag.Node
+		for i := 0; i < tt.forwardSteps; i++ {
+			y = model.NewProc(g).Forward(x...)
+			g.Clear()
+		}
+
+		require.Equal(t, len(x), len(y))
+
+		for i, v := range model.Mean.Value().Data() {
+			if !floats.EqualWithinAbs(v, tt.expectedAvg, 1e-1) {
+				t.Fatalf("Momentum %f Mean %d: expected zero, go %f", tt.momentum, i, v)
+			}
+		}
+
+		for i, v := range model.StdDev.Value().Data() {
+			if !floats.EqualWithinAbs(v, tt.expectedStdDev, 1e-1) {
+				t.Fatalf("Momentum %f StdDev %d: expected %f, got %f", tt.momentum, i, tt.expectedStdDev, v)
+			}
+		}
+	}
+
+}
+
+func TestModel_Inference(t *testing.T) {
+
+	model := New(3)
+	model.Mean = nn.NewParam(mat.NewVecDense([]float64{0.0, 0.0, 1.0}))
+	model.StdDev = nn.NewParam(mat.NewVecDense([]float64{1.0, 0.5, 1.0}))
+	g := ag.NewGraph()
+	proc := model.NewProc(g)
+	proc.SetMode(nn.Inference)
+	data := []float64{1.0, 2.0, 3.0}
+	x := g.NewVariable(mat.NewVecDense(data), false)
+	y := proc.Forward(x)
+	require.Equal(t, 1, len(y))
+
+	require.True(t, floats.EqualApprox(y[0].Value().Data(), []float64{1.0, 4.0, 2.0}, 1e-3))
+
+}
+
+func Test_Serialize(t *testing.T) {
+	model := NewWithMomentum(3, 0.777)
+	model.Mean = nn.NewParam(mat.NewVecDense([]float64{0.0, 0.0, 1.0}))
+	model.StdDev = nn.NewParam(mat.NewVecDense([]float64{1.0, 0.5, 1.0}))
+	tempFile, err := ioutil.TempFile("", "test_serialize")
+	require.Nil(t, err)
+	tempFile.Close()
+	defer os.Remove(tempFile.Name())
+	err = utils.SerializeToFile(tempFile.Name(), nn.NewParamsSerializer(model))
+	require.Nil(t, err)
+
+	model2 := New(3)
+	serializer := nn.NewParamsSerializer(model2)
+	tempFile, err = os.Open(tempFile.Name())
+	require.Nil(t, err)
+	_, err = serializer.Deserialize(tempFile)
+	require.NoError(t, err)
+	require.Equal(t, model.Momentum.Value().Scalar(), model2.Momentum.Value().Scalar())
+	require.Equal(t, model.Mean.Value().Data(), model2.Mean.Value().Data())
+	require.Equal(t, model.StdDev.Value().Data(), model2.StdDev.Value().Data())
+}
 func TestModel_Forward(t *testing.T) {
 
 	model := newTestModel()
