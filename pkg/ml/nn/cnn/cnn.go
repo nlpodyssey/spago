11c516ce752c59383562c6dc03aa8519581180e4
matteo-grella <matteogrella@gmail.com>
2020-12-04 12:04:35 +0100

    Code refactoring

--- pkg/ml/nn/cnn/cnn.go
+++ pkg/ml/nn/cnn/cnn.go
@@ -9,6 +9,7 @@ import (
 	"github.com/nlpodyssey/spago/pkg/ml/nn"
 	"github.com/nlpodyssey/spago/pkg/ml/nn/convolution"
 	"github.com/nlpodyssey/spago/pkg/ml/nn/linear"
+	"github.com/nlpodyssey/spago/pkg/ml/nn/pooling"
 )
 
 var (
@@ -18,28 +19,25 @@ var (
 
 // Model contains the serializable parameters.
 type Model struct {
-	Convolution    *convolution.Model
-	FinalLayer     *linear.Model
-	maxPoolingRows int
-	maxPoolingCols int
+	Convolution *convolution.Model
+	MaxPooling  *pooling.MaxPooling
+	FinalLayer  *linear.Model
 }
 
 // NewModel returns a new model with parameters initialized to zeros.
-func NewModel(convolution *convolution.Model, maxPoolingRows, maxPoolingCols int, finalLayer *linear.Model) *Model {
+func NewModel(convolution *convolution.Model, pooling *pooling.MaxPooling, finalLayer *linear.Model) *Model {
 	return &Model{
-		Convolution:    convolution,
-		FinalLayer:     finalLayer,
-		maxPoolingRows: maxPoolingRows,
-		maxPoolingCols: maxPoolingCols,
+		Convolution: convolution,
+		MaxPooling:  pooling,
+		FinalLayer:  finalLayer,
 	}
 }
 
 type Processor struct {
 	nn.BaseProcessor
-	maxPoolingRows int
-	maxPoolingCols int
-	Convolution    nn.Processor
-	FinalLayer     nn.Processor
+	Convolution *convolution.Processor
+	Pooling     *pooling.Processor
+	FinalLayer  *linear.Processor
 }
 
 // NewProc returns a new processor to execute the forward step.
@@ -51,10 +49,9 @@ func (m *Model) NewProc(g *ag.Graph) nn.Processor {
 			Graph:             g,
 			FullSeqProcessing: true,
 		},
-		maxPoolingRows: m.maxPoolingRows,
-		maxPoolingCols: m.maxPoolingCols,
-		Convolution:    m.Convolution.NewProc(g),
-		FinalLayer:     m.FinalLayer.NewProc(g),
+		Pooling:     m.MaxPooling.NewProc(g).(*pooling.Processor),
+		Convolution: m.Convolution.NewProc(g).(*convolution.Processor),
+		FinalLayer:  m.FinalLayer.NewProc(g).(*linear.Processor),
 	}
 }
 
@@ -62,22 +59,15 @@ func (p *Processor) SetMode(mode nn.ProcessingMode) {
 	p.Mode = mode
 	p.Convolution.SetMode(mode)
 	p.FinalLayer.SetMode(mode)
+	p.Pooling.SetMode(mode)
 }
 
 // Forward performs the forward step for each input and returns the result.
 func (p *Processor) Forward(xs ...ag.Node) []ag.Node {
 	filters := p.Convolution.Forward(xs...)
-	poolingFilters := p.maxPooling(filters...)
+	poolingFilters := p.Pooling.Forward(filters...)
 	concatFilters := p.Graph.Concat(p.vectorize(poolingFilters...)...)
 	return p.FinalLayer.Forward(concatFilters)
-}
-
-func (p *Processor) maxPooling(xs ...ag.Node) []ag.Node {
-	ret := make([]ag.Node, len(xs))
-	for i, x := range xs {
-		ret[i] = p.Graph.MaxPooling(x, p.maxPoolingRows, p.maxPoolingCols)
-	}
-	return ret
 }
 
 func (p *Processor) vectorize(xs ...ag.Node) []ag.Node {
