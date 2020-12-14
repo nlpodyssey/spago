c38372f58b4e0338fa9856151d09fd6da7e9b20c
matteo-grella <matteogrella@gmail.com>
2020-12-14 14:57:34 +0100

    Resolve TODO (implement classification algorithm)

--- pkg/nlp/transformers/bart/bartserver/server.go
+++ pkg/nlp/transformers/bart/bartserver/server.go
@@ -40,5 +40,51 @@ func (s *ServerForSequenceClassification) StartDefaultServer(grpcAddress, tlsCer
 
 // Classify handles a classification request over gRPC.
 func (s *ServerForSequenceClassification) Classify(_ context.Context, req *grpcapi.ClassifyRequest) (*grpcapi.ClassifyReply, error) {
-	return nil, nil // TODO: implement classification algorithm
+	result := s.classify(req.GetText(), req.GetText2())
+	return classificationFrom(result), nil
+}
+
+type ClassConfidencePair struct {
+	Class      string  `json:"class"`
+	Confidence float64 `json:"confidence"`
+}
+
+type ClassifyResponse struct {
+	Class        string                `json:"class"`
+	Confidence   float64               `json:"confidence"`
+	Distribution []ClassConfidencePair `json:"distribution"`
+	// Took is the number of milliseconds it took the server to execute the request.
+	Took int64 `json:"took"`
+}
+
+func classificationFrom(resp *ClassifyResponse) *grpcapi.ClassifyReply {
+	distribution := make([]*grpcapi.ClassConfidencePair, len(resp.Distribution))
+	for i, t := range resp.Distribution {
+		distribution[i] = &grpcapi.ClassConfidencePair{
+			Class:      t.Class,
+			Confidence: t.Confidence,
+		}
+	}
+	return &grpcapi.ClassifyReply{
+		Class:        resp.Class,
+		Confidence:   resp.Confidence,
+		Distribution: distribution,
+		Took:         resp.Took,
+	}
+}
+
+const (
+	defaultStartSequenceTokenID = 0
+	defaultEndSequenceTokenID   = 2
+)
+
+func getInputIDs(tokenizer *bpetokenizer.BPETokenizer, text, text2 string) []int {
+	encoded, _ := tokenizer.Encode(text) // TODO: error handling
+	inputIds := append(append([]int{defaultStartSequenceTokenID}, encoded.IDs...), defaultEndSequenceTokenID)
+	if text2 != "" {
+		encoded2, _ := tokenizer.Encode(text2) // TODO: error handling
+		inputIds2 := append(append([]int{defaultEndSequenceTokenID}, encoded2.IDs...), defaultEndSequenceTokenID)
+		inputIds = append(inputIds, inputIds2...)
+	}
+	return inputIds
 }
---
+++ pkg/nlp/transformers/bart/bartserver/server_classify.go
@@ -0,0 +1,49 @@
+// Copyright 2020 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bartserver
+
+import (
+	"github.com/nlpodyssey/spago/pkg/mat/f64utils"
+	"github.com/nlpodyssey/spago/pkg/ml/ag"
+	"github.com/nlpodyssey/spago/pkg/ml/nn"
+	"github.com/nlpodyssey/spago/pkg/nlp/transformers/bart/barthead"
+	"sort"
+	"strconv"
+	"time"
+)
+
+func (s *ServerForSequenceClassification) classify(text string, text2 string) *ClassifyResponse {
+	start := time.Now()
+
+	g := ag.NewGraph()
+	defer g.Clear()
+	proc := s.model.NewProc(nn.Context{Graph: g, Mode: nn.Inference}).(*barthead.SequenceClassificationProcessor)
+	inputIds := getInputIDs(s.tokenizer, text, text2)
+	logits := proc.Predict(inputIds...)[0]
+
+	probs := f64utils.SoftMax(logits.Value().Data())
+	best := f64utils.ArgMax(probs)
+	classes := s.model.BART.Config.ID2Label
+	class := classes[strconv.Itoa(best)]
+
+	distribution := make([]ClassConfidencePair, len(probs))
+	for i := 0; i < len(probs); i++ {
+		distribution[i] = ClassConfidencePair{
+			Class:      classes[strconv.Itoa(i)],
+			Confidence: probs[i],
+		}
+	}
+
+	sort.Slice(distribution, func(i, j int) bool {
+		return distribution[i].Confidence > distribution[j].Confidence
+	})
+
+	return &ClassifyResponse{
+		Class:        class,
+		Confidence:   probs[best],
+		Distribution: distribution,
+		Took:         time.Since(start).Milliseconds(),
+	}
+}
