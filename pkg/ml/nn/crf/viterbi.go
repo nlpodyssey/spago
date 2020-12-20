9bfce733aad9cff8a15f2bc3b4dbb4bfc9ae64d5
marco-nicola <marconicola@disroot.org>
2020-12-20 22:30:51 +0100

    Change visibility of some functions in ml/nn/crf/viterbi.go

--- pkg/ml/nn/crf/viterbi.go
+++ pkg/ml/nn/crf/viterbi.go
@@ -28,11 +28,11 @@ func NewViterbiStructure(size int) *ViterbiStructure {
 // Viterbi decodes the xs sequence according to the transitionMatrix.
 func Viterbi(transitionMatrix mat.Matrix, xs []ag.Node) []int {
 	alpha := make([]*ViterbiStructure, len(xs)+1)
-	alpha[0] = ViterbiStepStart(transitionMatrix, xs[0].Value())
+	alpha[0] = viterbiStepStart(transitionMatrix, xs[0].Value())
 	for i := 1; i < len(xs); i++ {
-		alpha[i] = ViterbiStep(transitionMatrix, alpha[i-1].scores, xs[i].Value())
+		alpha[i] = viterbiStep(transitionMatrix, alpha[i-1].scores, xs[i].Value())
 	}
-	alpha[len(xs)] = ViterbiStepEnd(transitionMatrix, alpha[len(xs)-1].scores)
+	alpha[len(xs)] = viterbiStepEnd(transitionMatrix, alpha[len(xs)-1].scores)
 
 	ys := make([]int, len(xs))
 	ys[len(xs)-1] = f64utils.ArgMax(alpha[len(xs)].scores.Data())
@@ -42,7 +42,7 @@ func Viterbi(transitionMatrix mat.Matrix, xs []ag.Node) []int {
 	return ys
 }
 
-func ViterbiStepStart(transitionMatrix mat.Matrix, maxVec mat.Matrix) *ViterbiStructure {
+func viterbiStepStart(transitionMatrix mat.Matrix, maxVec mat.Matrix) *ViterbiStructure {
 	y := NewViterbiStructure(transitionMatrix.Rows() - 1)
 	for i := 0; i < transitionMatrix.Rows()-1; i++ {
 		score := maxVec.At(i, 0) + transitionMatrix.At(0, i+1)
@@ -54,7 +54,7 @@ func ViterbiStepStart(transitionMatrix mat.Matrix, maxVec mat.Matrix) *ViterbiSt
 	return y
 }
 
-func ViterbiStepEnd(transitionMatrix mat.Matrix, maxVec mat.Matrix) *ViterbiStructure {
+func viterbiStepEnd(transitionMatrix mat.Matrix, maxVec mat.Matrix) *ViterbiStructure {
 	y := NewViterbiStructure(transitionMatrix.Rows() - 1)
 	for i := 0; i < transitionMatrix.Rows()-1; i++ {
 		score := maxVec.At(i, 0) + transitionMatrix.At(i+1, 0)
@@ -66,7 +66,7 @@ func ViterbiStepEnd(transitionMatrix mat.Matrix, maxVec mat.Matrix) *ViterbiStru
 	return y
 }
 
-func ViterbiStep(transitionMatrix mat.Matrix, maxVec mat.Matrix, stepVec mat.Matrix) *ViterbiStructure {
+func viterbiStep(transitionMatrix mat.Matrix, maxVec mat.Matrix, stepVec mat.Matrix) *ViterbiStructure {
 	y := NewViterbiStructure(transitionMatrix.Rows() - 1)
 	for i := 0; i < transitionMatrix.Rows()-1; i++ {
 		for j := 0; j < transitionMatrix.Columns()-1; j++ {
