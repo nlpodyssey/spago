9735e8670fa0afb6ae07b16b5772729eda831c19
marco-nicola <marconicola@disroot.org>
2021-01-02 11:33:05 +0100

    Create Float type in mat package

---
+++ pkg/mat/mat.go
@@ -0,0 +1,8 @@
+// Copyright 2021 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mat
+
+// Float is the main float type for the mat package. It is an alias for float64.
+type Float = float64
