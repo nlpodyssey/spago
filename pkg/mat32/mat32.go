fb032d01e89bc7510dd10d0c73b7032f26c6e389
marco-nicola <marconicola@disroot.org>
2021-01-02 11:46:56 +0100

    Create Float type in mat32 package

---
+++ pkg/mat32/mat32.go
@@ -0,0 +1,8 @@
+// Copyright 2021 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mat32
+
+// Float is the main float type for the mat32 package. It is an alias for float32.
+type Float = float32
