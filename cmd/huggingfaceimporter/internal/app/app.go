7e2df7349b63318b59fbe1a0f7d006b3a934d794
marco-nicola <marconicola@disroot.org>
2021-01-11 23:28:29 +0100

    Refactor huggingfaceimporter command implementation
    to make it consistent with all other commands

---
+++ cmd/huggingfaceimporter/internal/app/app.go
@@ -0,0 +1,24 @@
+// Copyright 2021 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package app
+
+import (
+	"github.com/nlpodyssey/spago/cmd/huggingfaceimporter/internal"
+	"github.com/urfave/cli"
+)
+
+// New returns a new CLI App for importing pickle-serialized Hugging Face models.
+func New() *cli.App {
+	importerArgs := internal.NewDefaultImporterArgs()
+	importerFlags := importerArgs.BuildFlags()
+
+	app := cli.NewApp()
+	app.Name = "huggingface-importer"
+	app.Usage = "Convert a pickle-serialized Hugging Face model to spaGO"
+	app.HideVersion = true
+	app.Action = importerArgs.RunImporterCli
+	app.Flags = importerFlags
+	return app
+}
