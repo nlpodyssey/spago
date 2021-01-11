de5f65f825f2b96d8cf54fd0e335b0e6df4ebea4
marco-nicola <marconicola@disroot.org>
2021-01-12 00:13:05 +0100

    Reimplement docker-entrypoint reusing the other cli apps
    instead of just calling separate executables. This will allow to build and use only a single tiny executable in the whole docker image.

--- docker-entrypoint.go
+++ docker-entrypoint.go
@@ -6,50 +6,60 @@ package main
 
 import (
 	"fmt"
+	bartapp "github.com/nlpodyssey/spago/cmd/bart/app"
+	bertapp "github.com/nlpodyssey/spago/cmd/bert/app"
+	huggingfaceimporterapp "github.com/nlpodyssey/spago/cmd/huggingfaceimporter/app"
+	nerapp "github.com/nlpodyssey/spago/cmd/ner/app"
+	"log"
 	"os"
-	"os/exec"
-	"strings"
-	"syscall"
 )
 
-const (
-	help = `
+const help = `
 spaGO is a self-contained ML/NLP library written in Go.
 
 Usage:
-  <command> [arguments]
+    <command> [arguments]
 
 The commands are:
 
-   bert-server             gRPC/HTTP server for BERT
-   bart-server             gRPC/HTTP server for BART
-   hugging-face-importer   Hugging Face model importing
-   ner-server              gRPC/HTTP server for Sequence Labeling
+    bert-server             gRPC/HTTP server for BERT
+    bart-server             gRPC/HTTP server for BART
+    huggingface-importer    Hugging Face model importing
+    ner-server              gRPC/HTTP server for Sequence Labeling
+    help                    print this help text and exit
 
-   See README.md for more information about run the demo servers using docker.
+See README.md for more information about run the demo servers using Docker.
 `
-)
 
 // The script docker-entrypoint.sh wraps access to the cmd programs.
 func main() {
+	logger := log.New(os.Stderr, "", 0)
 
-	// The help screen is printed to the user when no commands
-	// are given, or when the command "help" is given.
-	if len(os.Args) == 1 || strings.EqualFold(os.Args[1], "help") {
-		fmt.Println(help)
-		os.Exit(0)
+	if len(os.Args) == 1 {
+		logger.Fatalf("Command missing.\n%s", help)
 	}
 
-	// Run the commands defined by the Dockerfile CMD directive or overrides.
-	proc := os.Args[1]
-	_, err := exec.LookPath(proc)
-	if err != nil {
-		panic(err)
-	}
-
+	command := os.Args[1]
 	args := os.Args[1:]
-	fmt.Printf("Running command: '%s'\n", strings.Join(args, " "))
-	if err := syscall.Exec(proc, args, os.Environ()); err != nil {
-		panic(err)
+
+	var err error
+
+	switch command {
+	case "help":
+		fmt.Print(help)
+	case "bert-server":
+		err = bertapp.NewBertApp().Run(args)
+	case "bart-server":
+		err = bartapp.NewBartApp().Run(args)
+	case "huggingface-importer":
+		err = huggingfaceimporterapp.New().Run(args)
+	case "ner-server":
+		err = nerapp.NewNERApp().Run(args)
+	default:
+		logger.Fatalf("Invalid command.\n%s", help)
+	}
+
+	if err != nil {
+		logger.Fatal(err)
 	}
 }
