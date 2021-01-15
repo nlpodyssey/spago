a8cc06cf9b74f522d975139ea5e717ec3f9d3235
marco-nicola <marconicola@disroot.org>
2021-01-16 00:20:28 +0100

    Add new test cases for kvdb

--- pkg/utils/kvdb/kvdb_test.go
+++ pkg/utils/kvdb/kvdb_test.go
@@ -14,7 +14,119 @@ import (
 	"testing"
 )
 
+func TestKeyValueDB_ReadOnlyAndForceNew(t *testing.T) {
+	t.Parallel()
+
+	dir := newTempDir(t, "spago-kvdb-test-")
+	defer os.RemoveAll(dir)
+
+	// Create a new DB and write something
+	db1 := NewDefaultKeyValueDB(Config{Path: dir, ReadOnly: false, ForceNew: true})
+
+	err := db1.Put([]byte{1}, []byte{2})
+	require.Nil(t, err)
+
+	err = db1.Close()
+	require.Nil(t, err)
+
+	// Reopen the same DB in read-only mode
+	db2 := NewDefaultKeyValueDB(Config{Path: dir, ReadOnly: true, ForceNew: false})
+
+	value, ok, err := db2.Get([]byte{1})
+	require.Nil(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, []byte{2}, value)
+
+	err = db2.Put([]byte{3}, []byte{4})
+	assert.NotNil(t, err)
+	value, ok, err = db2.Get([]byte{3})
+	require.Nil(t, err)
+	assert.False(t, ok)
+	assert.Nil(t, value)
+
+	err = db2.Close()
+	require.Nil(t, err)
+
+	// Reopen a new DB reusing the same folder and forcing cleanup
+	db3 := NewDefaultKeyValueDB(Config{Path: dir, ReadOnly: false, ForceNew: true})
+
+	value, ok, err = db3.Get([]byte{1})
+	require.Nil(t, err)
+	assert.False(t, ok)
+	assert.Nil(t, value)
+
+	err = db3.Close()
+	require.Nil(t, err)
+}
+
+func TestKeyValueDB_DropAll(t *testing.T) {
+	t.Parallel()
+
+	dir := newTempDir(t, "spago-kvdb-test-")
+	defer os.RemoveAll(dir)
+
+	db := NewDefaultKeyValueDB(Config{Path: dir, ReadOnly: false, ForceNew: true})
+	defer db.Close()
+
+	err := db.Put([]byte{1}, []byte{2})
+	require.Nil(t, err)
+	err = db.Put([]byte{3}, []byte{4})
+	require.Nil(t, err)
+
+	keys, err := db.Keys()
+	require.Nil(t, err)
+	assert.NotEmpty(t, keys)
+
+	err = db.DropAll()
+	require.Nil(t, err)
+
+	keys, err = db.Keys()
+	require.Nil(t, err)
+	assert.Empty(t, keys)
+}
+
+func TestKeyValueDB_Keys(t *testing.T) {
+	t.Parallel()
+
+	dir := newTempDir(t, "spago-kvdb-test-")
+	defer os.RemoveAll(dir)
+
+	db := NewDefaultKeyValueDB(Config{Path: dir, ReadOnly: false, ForceNew: true})
+	defer db.Close()
+
+	err := db.Put([]byte{1}, []byte{2})
+	require.Nil(t, err)
+
+}
+
+func TestKeyValueDB_PutAndGet(t *testing.T) {
+	t.Parallel()
+
+	dir := newTempDir(t, "spago-kvdb-test-")
+	defer os.RemoveAll(dir)
+
+	db := NewDefaultKeyValueDB(Config{Path: dir, ReadOnly: false, ForceNew: true})
+	defer db.Close()
+
+	err := db.Put([]byte{1}, []byte{2})
+	require.Nil(t, err)
+
+	// Existing key
+	value, ok, err := db.Get([]byte{1})
+	require.Nil(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, []byte{2}, value)
+
+	// Nonexistent key
+	value, ok, err = db.Get([]byte{9})
+	require.Nil(t, err)
+	assert.False(t, ok)
+	assert.Nil(t, value)
+}
+
 func TestKeyValueDB_Gob(t *testing.T) {
+	t.Parallel()
+
 	var buf bytes.Buffer
 
 	// Setup first DB
