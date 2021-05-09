58c07695759d9bc6cb5e6329fa11897304bd5156
matteo-grella <matteogrella@gmail.com>
2021-05-09 10:17:56 +0200

    Code refactoring

--- pkg/nlp/transformers/bert/server_encode.go
+++ pkg/nlp/transformers/bert/server_encode.go
@@ -7,16 +7,11 @@ package bert
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	mat "github.com/nlpodyssey/spago/pkg/mat32"
 	"github.com/nlpodyssey/spago/pkg/nlp/transformers/bert/grpcapi"
 	"net/http"
-	"runtime"
 	"time"
-
-	"github.com/nlpodyssey/spago/pkg/ml/ag"
-	"github.com/nlpodyssey/spago/pkg/ml/nn"
-	"github.com/nlpodyssey/spago/pkg/nlp/tokenizers"
-	"github.com/nlpodyssey/spago/pkg/nlp/tokenizers/wordpiecetokenizer"
 )
 
 // SentenceEncoderHandler handles a sentence encoding request over HTTP.
@@ -31,7 +26,11 @@ func (s *Server) SentenceEncoderHandler(w http.ResponseWriter, req *http.Request
 		return
 	}
 
-	result := s.encode(body.Text, body.PoolingStrategy)
+	result, err := s.encode(body.Text, body.PoolingStrategy)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	_, pretty := req.URL.Query()["pretty"]
 	response, err := Dump(result, pretty)
 	if err != nil {
@@ -56,7 +55,10 @@ type EncodeResponse struct {
 // Encode handles an encoding request over gRPC.
 // TODO(evanmcclure@gmail.com) Reuse the gRPC message type for HTTP requests.
 func (s *Server) Encode(_ context.Context, req *grpcapi.EncodeRequest) (*grpcapi.EncodeReply, error) {
-	result := s.encode(req.GetText(), req.GetPoolingStrategy())
+	result, err := s.encode(req.GetText(), req.GetPoolingStrategy())
+	if err != nil {
+		return nil, err
+	}
 
 	vector32 := make([]float32, len(result.Data))
 	for i, num := range result.Data {
@@ -69,42 +71,33 @@ func (s *Server) Encode(_ context.Context, req *grpcapi.EncodeRequest) (*grpcapi
 	}, nil
 }
 
-func (s *Server) encode(text string, poolingStrategy grpcapi.EncodeRequest_PoolingStrategy) *EncodeResponse {
+func (s *Server) encode(text string, poolingStrategy grpcapi.EncodeRequest_PoolingStrategy) (*EncodeResponse, error) {
 	start := time.Now()
-	tokenizer := wordpiecetokenizer.New(s.model.Vocabulary)
-	origTokens := tokenizer.Tokenize(text)
-	tokenized := pad(tokenizers.GetStrings(origTokens))
-
-	g := ag.NewGraph(ag.ConcurrentComputations(runtime.NumCPU()))
-	defer g.Clear()
-	proc := nn.Reify(nn.Context{Graph: g, Mode: nn.Inference}, s.model).(*Model)
-	encoded := proc.Encode(tokenized)
-
-	var pooled ag.Node
-	switch poolingStrategy {
-	case grpcapi.EncodeRequest_REDUCE_MEAN:
-		pooled = g.Mean(encoded)
-	case grpcapi.EncodeRequest_REDUCE_MAX:
-		pooled = Max(g, encoded)
-	case grpcapi.EncodeRequest_REDUCE_MEAN_MAX:
-		pooled = g.Concat(g.Mean(encoded), Max(g, encoded))
-	case grpcapi.EncodeRequest_CLS_TOKEN:
-		pooled = proc.Pool(encoded)
-	default:
-		panic("bert: invalid pooling strategy")
+	ps, err := getPoolingStrategyFromEncodeRequest(poolingStrategy)
+	if err != nil {
+		return nil, err
 	}
-
+	encoded, err := s.model.Vectorize(text, ps)
+	if err != nil {
+		return nil, err
+	}
 	return &EncodeResponse{
-		Data: pooled.Value().Data(),
+		Data: encoded.Data(),
 		Took: time.Since(start).Milliseconds(),
-	}
+	}, nil
 }
 
-// Max returns the value that describes the maximum of the sample.
-func Max(g *ag.Graph, xs []ag.Node) ag.Node {
-	maxVector := xs[0]
-	for i := 1; i < len(xs); i++ {
-		maxVector = g.Max(maxVector, xs[i])
+func getPoolingStrategyFromEncodeRequest(poolingStrategy grpcapi.EncodeRequest_PoolingStrategy) (PoolingStrategy, error) {
+	switch poolingStrategy {
+	case grpcapi.EncodeRequest_REDUCE_MEAN:
+		return ReduceMean, nil
+	case grpcapi.EncodeRequest_REDUCE_MAX:
+		return ReduceMax, nil
+	case grpcapi.EncodeRequest_REDUCE_MEAN_MAX:
+		return ReduceMeanMax, nil
+	case grpcapi.EncodeRequest_CLS_TOKEN:
+		return ClsToken, nil
+	default:
+		return -1, fmt.Errorf("bert: invalid pooling strategy")
 	}
-	return maxVector
 }
---
+++ pkg/nlp/transformers/bert/vectorize.go
@@ -0,0 +1,66 @@
+// Copyright 2020 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bert
+
+import (
+	"fmt"
+	mat "github.com/nlpodyssey/spago/pkg/mat32"
+	"github.com/nlpodyssey/spago/pkg/ml/ag"
+	"github.com/nlpodyssey/spago/pkg/ml/nn"
+	"github.com/nlpodyssey/spago/pkg/nlp/tokenizers"
+	"github.com/nlpodyssey/spago/pkg/nlp/tokenizers/wordpiecetokenizer"
+	"runtime"
+)
+
+// PoolingStrategy defines the method to obtain the dense sentence representation
+type PoolingStrategy int
+
+const (
+	// ClsToken gets the encoding state corresponding to [CLS], i.e. the first token (default)
+	ClsToken PoolingStrategy = iota
+	// ReduceMean takes the average of the encoding states
+	ReduceMean
+	// ReduceMax takes the maximum of the encoding states
+	ReduceMax
+	// ReduceMeanMax does ReduceMean and ReduceMax separately and then concat them together
+	ReduceMeanMax
+)
+
+// Vectorize transforms the text into a dense vector representation.
+func (m *Model) Vectorize(text string, poolingStrategy PoolingStrategy) (mat.Matrix, error) {
+	tokenizer := wordpiecetokenizer.New(m.Vocabulary)
+	origTokens := tokenizer.Tokenize(text)
+	tokenized := pad(tokenizers.GetStrings(origTokens))
+
+	g := ag.NewGraph(ag.ConcurrentComputations(runtime.NumCPU()))
+	defer g.Clear()
+	proc := nn.Reify(nn.Context{Graph: g, Mode: nn.Inference}, m).(*Model)
+	encoded := proc.Encode(tokenized)
+
+	var pooled ag.Node
+	switch poolingStrategy {
+	case ReduceMean:
+		pooled = g.Mean(encoded)
+	case ReduceMax:
+		pooled = max(g, encoded)
+	case ReduceMeanMax:
+		pooled = g.Concat(g.Mean(encoded), max(g, encoded))
+	case ClsToken:
+		pooled = proc.Pool(encoded)
+	default:
+		return nil, fmt.Errorf("bert: invalid pooling strategy")
+	}
+
+	return g.GetCopiedValue(pooled), nil
+}
+
+// Max returns the value that describes the maximum of the sample.
+func max(g *ag.Graph, xs []ag.Node) ag.Node {
+	maxVector := xs[0]
+	for i := 1; i < len(xs); i++ {
+		maxVector = g.Max(maxVector, xs[i])
+	}
+	return maxVector
+}
