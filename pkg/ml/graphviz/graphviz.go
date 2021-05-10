2ce1651e075eaca730744472a90bdb7d6ddc22e3
Marco Nicola <m.nicola@exop-group.com>
2021-05-10 19:51:50 +0200

    Create graphviz.Save()

--- pkg/ml/graphviz/graphviz.go
+++ pkg/ml/graphviz/graphviz.go
@@ -7,9 +7,28 @@ package graphviz
 import (
 	"github.com/awalterschulze/gographviz"
 	"github.com/nlpodyssey/spago/pkg/ml/ag"
+	"os"
 )
 
 // BuildGraph creates a gographviz representation of the Graph.
 func BuildGraph(g *ag.Graph, options Options) (gographviz.Interface, error) {
 	return newBuilder(g, options).build()
+}
+
+// Save saves a gographviz graph to a DOT file.
+func Save(gv gographviz.Interface, filename string) (err error) {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := f.Close(); e != nil && err == nil {
+			err = e
+		}
+	}()
+	_, err = f.WriteString(gv.String())
+	if err != nil {
+		return err
+	}
+	return nil
 }
