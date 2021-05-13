5453974f6b90cbb7f9cdec8c5791a4192716fa89
Marco Nicola <marconicola@disroot.org>
2021-05-13 20:37:47 +0200

    Rename BaseModel.ModelGraph to G

--- pkg/ml/nn/basemodel.go
+++ pkg/ml/nn/basemodel.go
@@ -12,8 +12,8 @@ import (
 // BaseModel satisfies some methods of the Model interface.
 // Don't use it directly; it is meant to be embedded in other processors to reduce the amount of boilerplate code.
 type BaseModel struct {
-	// ModelGraph is the computational graph on which the (reified) model operates.
-	ModelGraph *ag.Graph
+	// G is the computational graph on which the (reified) model operates.
+	G *ag.Graph
 	// ProcessingMode is the processing mode for the model (training or inference).
 	ProcessingMode ProcessingMode
 }
@@ -30,16 +30,16 @@ func (m *BaseModel) Mode() ProcessingMode {
 // Graph returns the computational graph on which the (reified) model operates.
 // It panics if the Graph is nil.
 func (m *BaseModel) Graph() *ag.Graph {
-	if m.ModelGraph == nil {
+	if m.G == nil {
 		panic("nn: attempting to access Graph on a not reified model. Hint: use nn.Reify().")
 	}
-	return m.ModelGraph
+	return m.G
 }
 
 // IsProcessor returns whether the model has been reified (i.e., contextualized to operate
 // on a graph) and can perform the Forward().
 func (m *BaseModel) IsProcessor() bool {
-	return m.ModelGraph != nil
+	return m.G != nil
 }
 
 // InitProcessor is used to initialize structures and data useful for the Forward().
