c2fc8bd52e243d8a4011ca30b9135402dd1635ab
Marco Nicola <marconicola@disroot.org>
2021-11-12 11:43:26 +0100

    Format code in batchnorm files

--- pkg/ml/nn/normalization/batchnorm/batchnorm.go
+++ pkg/ml/nn/normalization/batchnorm/batchnorm.go
@@ -35,7 +35,7 @@ func init() {
 // NewWithMomentum returns a new model with supplied size and momentum.
 func NewWithMomentum(size int, momentum mat.Float) *Model {
 	return &Model{
-		W:        nn.NewParam(mat.NewInitVecDense(size,epsilon)),
+		W:        nn.NewParam(mat.NewInitVecDense(size, epsilon)),
 		B:        nn.NewParam(mat.NewEmptyVecDense(size)),
 		Mean:     nn.NewParam(mat.NewEmptyVecDense(size), nn.RequiresGrad(false)),
 		StdDev:   nn.NewParam(mat.NewEmptyVecDense(size), nn.RequiresGrad(false)),
--- pkg/ml/nn/normalization/batchnorm/batchnorm_test.go
+++ pkg/ml/nn/normalization/batchnorm/batchnorm_test.go
@@ -129,7 +129,7 @@ func TestModel_Inference(t *testing.T) {
 	model := New(3)
 	model.Mean = nn.NewParam(mat.NewVecDense([]mat.Float{0.0, 0.0, 1.0}))
 	model.StdDev = nn.NewParam(mat.NewVecDense([]mat.Float{1.0, 0.5, 1.0}))
-	model.W=nn.NewParam(mat.NewInitVecDense(3,1.0))
+	model.W = nn.NewParam(mat.NewInitVecDense(3, 1.0))
 	g := ag.NewGraph()
 	proc := nn.ReifyForInference(model, g)
 	data := []mat.Float{1.0, 2.0, 3.0}
