663e569e77333cf6d1263fb7bee7c341a4eac8cc
Marco Nicola <marconicola@disroot.org>
2022-02-08 21:19:08 +0100

    Rewrite ml/encodding/fofe.Decode test

--- pkg/ml/encoding/fofe/decoder_test.go
+++ pkg/ml/encoding/fofe/decoder_test.go
@@ -4,26 +4,37 @@
 
 package fofe
 
-// TODO: rewrite this test
-//func TestDecode(t *testing.T) {
-//	reverseVocabulary := map[int]string{0: "a", 1: "b", 2: "c", 3: "d", 4: "e"}
-//
-//	z := mat.NewSparseFromMap(len(reverseVocabulary), 1, map[mat.Coordinate]mat.Float{
-//		{I: 0, J: 0}: 1.00781250,
-//		{I: 2, J: 0}: 0.51562500,
-//		{I: 3, J: 0}: 0.28125000,
-//		{I: 4, J: 0}: 0.18750000,
-//	})
-//
-//	decoding := Decode(0.5, z)
-//
-//	var xs string
-//	for _, id := range decoding {
-//		c, _ := reverseVocabulary[id]
-//		xs += c
-//	}
-//
-//	if xs != "acdeedca" {
-//		t.Errorf("The decoded string doesn't match the expected value.")
-//	}
-//}
+import (
+	"github.com/nlpodyssey/spago/pkg/mat"
+	"testing"
+)
+
+func TestDecode(t *testing.T) {
+	reverseVocabulary := []string{
+		0: "a",
+		1: "b",
+		2: "c",
+		3: "d",
+		4: "e",
+	}
+
+	z := mat.NewVecDense([]mat.Float{
+		0: 1.00781250,
+		1: 0,
+		2: 0.51562500,
+		3: 0.28125000,
+		4: 0.18750000,
+	})
+
+	decoding := Decode(0.5, z)
+
+	var xs string
+	for _, id := range decoding {
+		c := reverseVocabulary[id]
+		xs += c
+	}
+
+	if xs != "acdeedca" {
+		t.Errorf("The decoded string doesn't match the expected value.")
+	}
+}
