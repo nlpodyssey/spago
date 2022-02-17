d6b36145dd7c0fccd58bc86a9da698a47f141485
Marco Nicola <marconicola@disroot.org>
2022-02-17 22:29:25 +0100

    Provide better implementation for mat Dense marshaling functions

--- mat/dense_marshaling.go
+++ mat/dense_marshaling.go
@@ -7,6 +7,7 @@ package mat
 import (
 	"encoding/binary"
 	"encoding/gob"
+	"errors"
 	"fmt"
 	"math"
 )
@@ -15,6 +16,11 @@ func init() {
 	gob.Register(&Dense[float32]{})
 	gob.Register(&Dense[float64]{})
 }
+
+const (
+	binaryDenseFloat32 byte = iota
+	binaryDenseFloat64
+)
 
 // MarshalBinary marshals a Dense matrix into binary form.
 func (d Dense[T]) MarshalBinary() ([]byte, error) {
@@ -26,26 +32,6 @@ func (d Dense[T]) MarshalBinary() ([]byte, error) {
 	default:
 		panic(fmt.Sprintf("mat: unexpected dense matrix type %T", T(0)))
 	}
-}
-
-func (d Dense[T]) marshalBinaryFloat32() ([]byte, error) {
-	data := make([]byte, 8+d.Size()*4)
-	binary.LittleEndian.PutUint32(data, uint32(d.rows))
-	binary.LittleEndian.PutUint32(data[4:], uint32(d.cols))
-	for i, v := range d.data {
-		binary.LittleEndian.PutUint32(data[8+i*4:], math.Float32bits(float32(v)))
-	}
-	return data, nil
-}
-
-func (d Dense[T]) marshalBinaryFloat64() ([]byte, error) {
-	data := make([]byte, 8+d.Size()*8)
-	binary.LittleEndian.PutUint32(data, uint32(d.rows))
-	binary.LittleEndian.PutUint32(data[4:], uint32(d.cols))
-	for i, v := range d.data {
-		binary.LittleEndian.PutUint64(data[8+i*8:], math.Float64bits(float64(v)))
-	}
-	return data, nil
 }
 
 // UnmarshalBinary unmarshals a binary representation of a Dense matrix.
@@ -60,34 +46,84 @@ func (d *Dense[T]) UnmarshalBinary(data []byte) error {
 	}
 }
 
+// Dense matrix - float32 marshaling:
+// - 1 byte - identifier binaryDenseFloat32 (byte)
+// - 8 bytes - rows (uint64)
+// - 8 bytes - cols (uint64)
+// - 4*size bytes - data (float32 as uint32-bits)
+
+func (d Dense[T]) marshalBinaryFloat32() ([]byte, error) {
+	data := make([]byte, 17+len(d.data)*4)
+	data[0] = binaryDenseFloat32
+	binary.LittleEndian.PutUint64(data[1:], uint64(d.rows))
+	binary.LittleEndian.PutUint64(data[9:], uint64(d.cols))
+
+	s := data[17:]
+	for _, v := range d.data {
+		binary.LittleEndian.PutUint32(s, math.Float32bits(float32(v)))
+		s = s[4:]
+	}
+
+	return data, nil
+}
+
 func (d *Dense[T]) unmarshalBinaryFloat32(data []byte) error {
-	d.flags = 0
-	d.rows = int(binary.LittleEndian.Uint32(data))
-	d.cols = int(binary.LittleEndian.Uint32(data[4:]))
+	if data[0] != binaryDenseFloat32 {
+		return errors.New("mat: cannot unmarshal Dense[float32]: invalid identifier")
+	}
+
+	d.rows = int(binary.LittleEndian.Uint64(data[1:]))
+	d.cols = int(binary.LittleEndian.Uint64(data[9:]))
 	size := d.rows * d.cols
 	d.data = make([]T, size)
 
-	if expected := 8 + size*4; len(data) != expected {
-		return fmt.Errorf("mat: cannot unmarshal float32 Dense matrix. Data len expected %d, actual %d", expected, len(data))
-	}
+	data = data[17:]
 	for i := range d.data {
-		d.data[i] = T(math.Float32frombits(binary.LittleEndian.Uint32(data[8+i*4:])))
+		d.data[i] = T(math.Float32frombits(binary.LittleEndian.Uint32(data)))
+		data = data[4:]
 	}
+
+	d.flags = 0
 	return nil
 }
 
+// Dense matrix - float64 marshaling:
+// - 1 byte - identifier binaryDenseFloat64 (byte)
+// - 8 bytes - rows (uint64)
+// - 8 bytes - cols (uint64)
+// - 8*size bytes - data (float64 as uint64-bits)
+
+func (d Dense[T]) marshalBinaryFloat64() ([]byte, error) {
+	data := make([]byte, 17+len(d.data)*8)
+	data[0] = binaryDenseFloat64
+	binary.LittleEndian.PutUint64(data[1:], uint64(d.rows))
+	binary.LittleEndian.PutUint64(data[9:], uint64(d.cols))
+
+	s := data[17:]
+	for _, v := range d.data {
+		binary.LittleEndian.PutUint64(s, math.Float64bits(float64(v)))
+		s = s[8:]
+	}
+
+	return data, nil
+}
+
 func (d *Dense[T]) unmarshalBinaryFloat64(data []byte) error {
-	d.flags = 0
-	d.rows = int(binary.LittleEndian.Uint32(data))
-	d.cols = int(binary.LittleEndian.Uint32(data[4:]))
+	if data[0] != binaryDenseFloat64 {
+		return errors.New("mat: cannot unmarshal Dense[float64]: invalid identifier")
+	}
+
+	d.rows = int(binary.LittleEndian.Uint64(data[1:]))
+	d.cols = int(binary.LittleEndian.Uint64(data[9:]))
 	size := d.rows * d.cols
 	d.data = make([]T, size)
 
-	if expected := 8 + size*8; len(data) != expected {
-		return fmt.Errorf("mat: cannot unmarshal float64 Dense matrix. Data len expected %d, actual %d", expected, len(data))
-	}
+	data = data[17:]
 	for i := range d.data {
-		d.data[i] = T(math.Float64frombits(binary.LittleEndian.Uint64(data[8+i*8:])))
+		d.data[i] = T(math.Float64frombits(binary.LittleEndian.Uint64(data)))
+		data = data[8:]
 	}
+
+	d.flags = 0
 	return nil
 }
