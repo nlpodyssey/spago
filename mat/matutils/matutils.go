6559aebc5b7f87ff7b10a3fd5f0f7e05d9a7aff7
Marco Nicola <marconicola@disroot.org>
2022-02-18 20:42:00 +0100

    Remove mat/matutils.CumSum()
    Using Matrix.VecCumSum() should be preferred.

--- mat/matutils/matutils.go
+++ mat/matutils/matutils.go
@@ -3,22 +3,3 @@
 // license that can be found in the LICENSE file.
 
 package matutils
-
-import (
-	"fmt"
-	"github.com/nlpodyssey/spago/mat"
-	"github.com/nlpodyssey/spago/mat/internal/f32"
-	"github.com/nlpodyssey/spago/mat/internal/f64/asm64"
-)
-
-// CumSum computes the cumulative sum of src into dst, and returns dst.
-func CumSum[T mat.DType](dst, src []T) []T {
-	switch any(T(0)).(type) {
-	case float32:
-		return any(f32.CumSum(any(dst).([]float32), any(src).([]float32))).([]T)
-	case float64:
-		return any(asm64.CumSum(any(dst).([]float64), any(src).([]float64))).([]T)
-	default:
-		panic(fmt.Sprintf("matutils: unexpected type %T", T(0)))
-	}
-}
