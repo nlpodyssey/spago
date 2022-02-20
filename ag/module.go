fbb402ef01ae0c85c892e0b265b172dc171af889
Matteo Grella <matteogrella@gmail.com>
2022-02-20 22:57:15 +0100

    Add `ag.Differentiable` interface and `ag.DifferentiableModule` implementation

---
+++ ag/module.go
@@ -0,0 +1,18 @@
+// Copyright 2022 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ag
+
+import "github.com/nlpodyssey/spago/mat"
+
+type Differentiable[T mat.DType] interface {
+	mustEmbedDifferentiableModule()
+}
+
+// DifferentiableModule must be embedded into all differentiable modules.
+type DifferentiableModule[T mat.DType] struct {
+	Graph *Graph[T]
+}
+
+func (m DifferentiableModule[T]) mustEmbedDifferentiableModule() {}
