bada01174f7ae26509dc8e8d2d2baefb6e427930
matteo-grella <matteogrella@gmail.com>
2022-04-03 00:52:53 +0200

    Code refactoring

--- gd/optimizer.go
+++ gd/optimizer.go
@@ -14,27 +14,24 @@ import (
 
 // Optimizer implements Gradients Descent (GD) optimization.
 type Optimizer[T mat.DType] struct {
-	method           Method[T] // optimization method (SGD, AdaGrad, Adam, ...)
-	gradClipper      clipper.GradClipper[T]
-	paramsGetter     nn.ParamsGetter[T]
-	paramsToOptimize []nn.Param[T]
-	// maxProc limits the number of concurrent params update operations (default runtime.NumCPU())
-	maxProc int
+	model       nn.Model[T] // model to optimize
+	method      Method[T]   // optimization method (SGD, AdaGrad, Adam, ...)
+	gradClipper clipper.GradClipper[T]
 }
 
 // Option allows to configure a new Optimizer with your specific needs.
 type Option[T mat.DType] func(*Optimizer[T])
 
-// ClipGradByValue is an option to clip the gradients during the training between
+// WithClipGradByValue is an option to clip the gradients during the training between
 // -value and +value.
-func ClipGradByValue[T mat.DType](value T) Option[T] {
+func WithClipGradByValue[T mat.DType](value T) Option[T] {
 	return func(f *Optimizer[T]) {
 		f.gradClipper = &clipper.ClipValue[T]{Value: value}
 	}
 }
 
-// ClipGradByNorm is an option to clip the gradients during the training by norm.
-func ClipGradByNorm[T mat.DType](max, normType T) Option[T] {
+// WithClipGradByNorm is an option to clip the gradients during the training by norm.
+func WithClipGradByNorm[T mat.DType](max, normType T) Option[T] {
 	return func(f *Optimizer[T]) {
 		f.gradClipper = &clipper.ClipNorm[T]{
 			MaxNorm:  max,
@@ -43,25 +40,11 @@ func ClipGradByNorm[T mat.DType](max, normType T) Option[T] {
 	}
 }
 
-// WithMaxProc sets the maximum number of concurrent computations handled by the Optimizer
-// for heavy tasks such as the params update steps.
-// The value 1 corresponds to sequential execution.
-func WithMaxProc[T mat.DType](value int) Option[T] {
-	if value < 1 {
-		panic("gd: value must be greater than zero")
-	}
-	return func(f *Optimizer[T]) {
-		f.maxProc = value
-	}
-}
-
-// NewOptimizer returns a new Optimizer. The gradient clipper can be set to nil.
-func NewOptimizer[T mat.DType](method Method[T], paramsIterator nn.ParamsGetter[T], opts ...Option[T]) *Optimizer[T] {
+// NewOptimizer returns a new Optimizer.
+func NewOptimizer[T mat.DType](model nn.Model[T], method Method[T], opts ...Option[T]) *Optimizer[T] {
 	optimizer := &Optimizer[T]{
-		method:           method,
-		paramsGetter:     paramsIterator,
-		paramsToOptimize: make([]nn.Param[T], 0),
-		maxProc:          runtime.NumCPU(),
+		model:  model,
+		method: method,
 	}
 	for _, opt := range opts {
 		opt(optimizer)
@@ -69,36 +52,33 @@ func NewOptimizer[T mat.DType](method Method[T], paramsIterator nn.ParamsGetter[
 	return optimizer
 }
 
-// Optimize optimizes the params, applying the optional gradient clipping.
+// Optimize optimizes the model parameters, applying the optional gradient clipping.
 // After the optimization the params have zero gradients.
-func (o *Optimizer[_]) Optimize() {
-	o.paramsToOptimize = o.paramsGetter.Params()
-	if o.paramsToOptimize == nil {
-		return
-	}
-	o.clipGrads()
-	o.updateParams()
-	o.paramsToOptimize = nil
+func (o *Optimizer[T]) Optimize() {
+	params := o.collectParams()
+	o.clipGradsInPlace(params)
+	o.updateParams(params)
 }
 
-// updateParamsSerial applies the optimization method to all the observed parameters.
-func (o *Optimizer[_]) updateParamsSerial() {
-	for _, param := range o.paramsToOptimize {
-		if param.HasGrad() {
-			delta := o.method.Delta(param) // important: don't release delta here
-			param.ApplyDelta(delta)
-			param.ZeroGrad()
-		}
-	}
-}
-
-// updateParams applies the optimization method to all the observed parameters concurrently.
-func (o *Optimizer[T]) updateParams() {
-	ch := make(chan struct{}, o.maxProc)
-	for _, param := range o.paramsToOptimize {
+func (o *Optimizer[T]) collectParams() []nn.Param[T] {
+	visited := map[nn.Param[T]]struct{}{}
+	params := make([]nn.Param[T], 0)
+	nn.ForEachParam(o.model, func(param nn.Param[T], _ string, _ nn.ParamsType) {
 		if !param.HasGrad() {
-			continue
+			return // don't consider params with grad at zero
 		}
+		if _, ok := visited[param]; !ok {
+			params = append(params, param)
+			visited[param] = struct{}{}
+		}
+	})
+	return params
+}
+
+// updateParams applies the optimization method to all the observed parameters.
+func (o *Optimizer[T]) updateParams(params []nn.Param[T]) {
+	ch := make(chan struct{}, runtime.NumCPU())
+	for _, param := range params {
 		ch <- struct{}{}
 		go func(p nn.Param[T]) {
 			delta := o.method.Delta(p)
@@ -114,15 +94,13 @@ func (o *Optimizer[T]) updateParams() {
 }
 
 // clipGrad applies the gradient clipping to all the observed parameters.
-func (o *Optimizer[T]) clipGrads() {
+func (o *Optimizer[T]) clipGradsInPlace(params []nn.Param[T]) {
 	if o.gradClipper == nil {
 		return
 	}
 	var gs []mat.Matrix[T]
-	for _, param := range o.paramsToOptimize {
-		if param.HasGrad() { // don't consider grad at zero
-			gs = append(gs, param.Grad())
-		}
+	for _, param := range params {
+		gs = append(gs, param.Grad())
 	}
 	o.gradClipper.Clip(gs)
 }
