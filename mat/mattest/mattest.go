46585e536cd30f75b33ee8eb15d1d227a4a9ca8f
Marco Nicola <marconicola@disroot.org>
2022-04-07 22:46:05 +0200

    Create package mat/mattest

---
+++ mat/mattest/mattest.go
@@ -0,0 +1,101 @@
+// Copyright 2022 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package mattest provides utilities for testing code involving spaGO matrices.
+package mattest
+
+import (
+	"fmt"
+
+	"github.com/nlpodyssey/spago/mat"
+)
+
+// T requires a subset of methods from testing.TB.
+// This interface is primarily useful to simplify the testing of the
+// package itself.
+type T interface {
+	Helper()
+	Error(args ...any)
+	Errorf(format string, args ...any)
+	FailNow()
+}
+
+// AssertMatrixEquals tests whether expected is equal to actual; if not,
+// T.Error or T.Errorf are called, providing useful information.
+//
+// It returns whether the comparison succeeded.
+//
+// The expected matrix is not allowed to be nil, otherwise the function always
+// produces an error.
+func AssertMatrixEquals[DT mat.DType](t T, expected, actual mat.Matrix[DT], args ...any) bool {
+	t.Helper()
+	if expected == nil {
+		t.Error("the expected matrix must not be nil")
+		return false
+	}
+	if actual == nil {
+		t.Errorf("Matrices are not equal:\nexpected:\n%g\nactual:\nnil\n%s",
+			expected, fmt.Sprint(args...))
+		return false
+	}
+	if !mat.Equal(expected, actual) {
+		t.Errorf("Matrices are not equal:\nexpected:\n%g\nactual:\n%g\n%s",
+			expected, actual, fmt.Sprint(args...))
+		return false
+	}
+	return true
+}
+
+// RequireMatrixEquals tests whether expected is equal to actual; if not,
+// T.Error or T.Errorf are called, providing useful information, followed by
+// T.FailNow.
+//
+// The expected matrix is not allowed to be nil, otherwise the function always
+// produces an error and fails.
+func RequireMatrixEquals[DT mat.DType](t T, expected, actual mat.Matrix[DT], args ...any) {
+	t.Helper()
+	if !AssertMatrixEquals(t, expected, actual, args...) {
+		t.FailNow()
+	}
+}
+
+// AssertMatrixInDelta tests whether expected and actual have the same shape
+// and all elements at the same positions are within delta; if not, T.Error
+// or T.Errorf are called, providing useful information.
+//
+// It returns whether the comparison succeeded.
+//
+// The expected matrix is not allowed to be nil, otherwise the function always
+// produces an error.
+func AssertMatrixInDelta[DT mat.DType](t T, expected, actual mat.Matrix[DT], delta DT, args ...any) bool {
+	t.Helper()
+	if expected == nil {
+		t.Error("the expected matrix must not be nil")
+		return false
+	}
+	if actual == nil {
+		t.Errorf("Matrices values are not within delta %g:\nexpected:\n%g\nactual:\nnil\n%s",
+			delta, expected, fmt.Sprint(args...))
+		return false
+	}
+	if !mat.InDelta(expected, actual, delta) {
+		t.Errorf("Matrices values are not within delta %g:\nexpected:\n%g\nactual:\n%g\n%s",
+			delta, expected, actual, fmt.Sprint(args...))
+		return false
+	}
+	return true
+}
+
+// RequireMatrixInDelta tests whether expected and actual have the same shape
+// and all elements at the same positions are within delta; if not, T.Error
+// or T.Errorf are called, providing useful information, followed by T.FailNow.
+//
+// The expected matrix is not allowed to be nil, otherwise the function always
+// produces an error and fails.
+func RequireMatrixInDelta[DT mat.DType](t T, expected, actual mat.Matrix[DT], delta DT, args ...any) {
+	t.Helper()
+	if !AssertMatrixInDelta(t, expected, actual, delta, args...) {
+		t.FailNow()
+	}
+}
---
+++ mat/mattest/mattest_test.go
@@ -0,0 +1,370 @@
+// Copyright 2022 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mattest_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/nlpodyssey/spago/mat"
+	"github.com/nlpodyssey/spago/mat/mattest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAssertMatrixEquals(t *testing.T) {
+	t.Run("float32", testAssertMatrixEquals[float32])
+	t.Run("float64", testAssertMatrixEquals[float64])
+}
+
+func testAssertMatrixEquals[T mat.DType](t *testing.T) {
+	for _, tc := range matrixEqualsTestCases[T]() {
+		t.Run(tc.name, func(t *testing.T) {
+			dt := new(dummyT)
+			if !tc.success {
+				dt.error = func(args ...any) {
+					assert.Equal(t, tc.errorMsg, fmt.Sprint(args...))
+				}
+				dt.errorf = func(format string, args ...any) {
+					assert.Equal(t, tc.errorMsg, fmt.Sprintf(format, args...))
+				}
+			}
+
+			mattest.AssertMatrixEquals[T](dt, tc.expected, tc.actual, tc.args...)
+
+			assert.Positive(t, dt.helperCalls)
+			assert.Equal(t, 0, dt.failNowCalls)
+			if tc.success {
+				assert.Equal(t, 0, dt.errorfCalls+dt.errorCalls)
+			} else {
+				assert.Equal(t, 1, dt.errorfCalls+dt.errorCalls)
+			}
+		})
+	}
+}
+
+func TestRequireMatrixEquals(t *testing.T) {
+	t.Run("float32", testRequireMatrixEquals[float32])
+	t.Run("float64", testRequireMatrixEquals[float64])
+}
+
+func testRequireMatrixEquals[T mat.DType](t *testing.T) {
+	for _, tc := range matrixEqualsTestCases[T]() {
+		t.Run(tc.name, func(t *testing.T) {
+			dt := new(dummyT)
+			if !tc.success {
+				dt.error = func(args ...any) {
+					assert.Equal(t, tc.errorMsg, fmt.Sprint(args...))
+				}
+				dt.errorf = func(format string, args ...any) {
+					assert.Equal(t, tc.errorMsg, fmt.Sprintf(format, args...))
+				}
+			}
+
+			mattest.RequireMatrixEquals[T](dt, tc.expected, tc.actual, tc.args...)
+
+			assert.Positive(t, dt.helperCalls)
+			if tc.success {
+				assert.Equal(t, 0, dt.errorfCalls+dt.errorCalls)
+				assert.Equal(t, 0, dt.failNowCalls)
+			} else {
+				assert.Equal(t, 1, dt.errorfCalls+dt.errorCalls)
+				assert.Equal(t, 1, dt.failNowCalls)
+			}
+		})
+	}
+}
+
+func TestAssertMatrixInDelta(t *testing.T) {
+	t.Run("float32", testAssertMatrixInDelta[float32])
+	t.Run("float64", testAssertMatrixInDelta[float64])
+}
+
+func testAssertMatrixInDelta[T mat.DType](t *testing.T) {
+	for _, tc := range matrixInDeltaTestCases[T]() {
+		t.Run(tc.name, func(t *testing.T) {
+			dt := new(dummyT)
+			if !tc.success {
+				dt.error = func(args ...any) {
+					assert.Equal(t, tc.errorMsg, fmt.Sprint(args...))
+				}
+				dt.errorf = func(format string, args ...any) {
+					assert.Equal(t, tc.errorMsg, fmt.Sprintf(format, args...))
+				}
+			}
+
+			mattest.AssertMatrixInDelta[T](dt, tc.expected, tc.actual, tc.delta, tc.args...)
+
+			assert.Positive(t, dt.helperCalls)
+			assert.Equal(t, 0, dt.failNowCalls)
+			if tc.success {
+				assert.Equal(t, 0, dt.errorfCalls+dt.errorCalls)
+			} else {
+				assert.Equal(t, 1, dt.errorfCalls+dt.errorCalls)
+			}
+		})
+	}
+}
+
+func TestRequireMatrixInDelta(t *testing.T) {
+	t.Run("float32", testRequireMatrixInDelta[float32])
+	t.Run("float64", testRequireMatrixInDelta[float64])
+}
+
+func testRequireMatrixInDelta[T mat.DType](t *testing.T) {
+	for _, tc := range matrixInDeltaTestCases[T]() {
+		t.Run(tc.name, func(t *testing.T) {
+			dt := new(dummyT)
+			if !tc.success {
+				dt.error = func(args ...any) {
+					assert.Equal(t, tc.errorMsg, fmt.Sprint(args...))
+				}
+				dt.errorf = func(format string, args ...any) {
+					assert.Equal(t, tc.errorMsg, fmt.Sprintf(format, args...))
+				}
+			}
+
+			mattest.RequireMatrixInDelta[T](dt, tc.expected, tc.actual, tc.delta, tc.args...)
+
+			assert.Positive(t, dt.helperCalls)
+			if tc.success {
+				assert.Equal(t, 0, dt.errorfCalls+dt.errorCalls)
+				assert.Equal(t, 0, dt.failNowCalls)
+			} else {
+				assert.Equal(t, 1, dt.errorfCalls+dt.errorCalls)
+				assert.Equal(t, 1, dt.failNowCalls)
+			}
+		})
+	}
+}
+
+type matrixEqualsTestCase[T mat.DType] struct {
+	name     string
+	expected mat.Matrix[T]
+	actual   mat.Matrix[T]
+	args     []any
+	success  bool
+	errorMsg string
+}
+
+func matrixEqualsTestCases[T mat.DType]() []matrixEqualsTestCase[T] {
+	return []matrixEqualsTestCase[T]{
+		{
+			name:     "no errors",
+			expected: mat.NewScalar[T](42),
+			actual:   mat.NewScalar[T](42),
+			args:     nil,
+			success:  true,
+			errorMsg: "",
+		},
+		{
+			name:     "expected nil",
+			expected: nil,
+			actual:   mat.NewScalar[T](42),
+			args:     nil,
+			success:  false,
+			errorMsg: "the expected matrix must not be nil",
+		},
+		{
+			name:     "actual nil",
+			expected: mat.NewScalar[T](42),
+			actual:   nil,
+			args:     nil,
+			success:  false,
+			errorMsg: strings.Join([]string{
+				"Matrices are not equal:",
+				"expected:",
+				"[42]",
+				"actual:",
+				"nil",
+				"",
+			}, "\n"),
+		},
+		{
+			name:     "actual nil - args",
+			expected: mat.NewScalar[T](42),
+			actual:   nil,
+			args:     []any{"foo ", 123},
+			success:  false,
+			errorMsg: strings.Join([]string{
+				"Matrices are not equal:",
+				"expected:",
+				"[42]",
+				"actual:",
+				"nil",
+				"foo 123",
+			}, "\n"),
+		},
+		{
+			name:     "matrices not equal",
+			expected: mat.NewScalar[T](42),
+			actual:   mat.NewScalar[T](0),
+			args:     nil,
+			success:  false,
+			errorMsg: strings.Join([]string{
+				"Matrices are not equal:",
+				"expected:",
+				"[42]",
+				"actual:",
+				"[0]",
+				"",
+			}, "\n"),
+		},
+		{
+			name:     "matrices not equal - args",
+			expected: mat.NewScalar[T](42),
+			actual:   mat.NewScalar[T](0),
+			args:     []any{"foo ", 123},
+			success:  false,
+			errorMsg: strings.Join([]string{
+				"Matrices are not equal:",
+				"expected:",
+				"[42]",
+				"actual:",
+				"[0]",
+				"foo 123",
+			}, "\n"),
+		},
+	}
+}
+
+type matrixInDeltaTestCase[T mat.DType] struct {
+	name     string
+	expected mat.Matrix[T]
+	actual   mat.Matrix[T]
+	delta    T
+	args     []any
+	success  bool
+	errorMsg string
+}
+
+func matrixInDeltaTestCases[T mat.DType]() []matrixInDeltaTestCase[T] {
+	return []matrixInDeltaTestCase[T]{
+		{
+			name:     "no errors",
+			expected: mat.NewScalar[T](42),
+			actual:   mat.NewScalar[T](43),
+			delta:    1,
+			args:     nil,
+			success:  true,
+			errorMsg: "",
+		},
+		{
+			name:     "expected nil",
+			expected: nil,
+			actual:   mat.NewScalar[T](42),
+			delta:    1,
+			args:     nil,
+			success:  false,
+			errorMsg: "the expected matrix must not be nil",
+		},
+		{
+			name:     "actual nil",
+			expected: mat.NewScalar[T](42),
+			actual:   nil,
+			delta:    1,
+			args:     nil,
+			success:  false,
+			errorMsg: strings.Join([]string{
+				"Matrices values are not within delta 1:",
+				"expected:",
+				"[42]",
+				"actual:",
+				"nil",
+				"",
+			}, "\n"),
+		},
+		{
+			name:     "actual nil - args",
+			expected: mat.NewScalar[T](42),
+			actual:   nil,
+			delta:    1,
+			args:     []any{"foo ", 123},
+			success:  false,
+			errorMsg: strings.Join([]string{
+				"Matrices values are not within delta 1:",
+				"expected:",
+				"[42]",
+				"actual:",
+				"nil",
+				"foo 123",
+			}, "\n"),
+		},
+		{
+			name:     "matrices not equal",
+			expected: mat.NewScalar[T](42),
+			actual:   mat.NewScalar[T](44),
+			delta:    1,
+			args:     nil,
+			success:  false,
+			errorMsg: strings.Join([]string{
+				"Matrices values are not within delta 1:",
+				"expected:",
+				"[42]",
+				"actual:",
+				"[44]",
+				"",
+			}, "\n"),
+		},
+		{
+			name:     "matrices not equal - args",
+			expected: mat.NewScalar[T](42),
+			actual:   mat.NewScalar[T](44),
+			delta:    1,
+			args:     []any{"foo ", 123},
+			success:  false,
+			errorMsg: strings.Join([]string{
+				"Matrices values are not within delta 1:",
+				"expected:",
+				"[42]",
+				"actual:",
+				"[44]",
+				"foo 123",
+			}, "\n"),
+		},
+	}
+}
+
+type dummyT struct {
+	helper      func()
+	helperCalls int
+
+	error      func(args ...any)
+	errorCalls int
+
+	errorf      func(format string, args ...any)
+	errorfCalls int
+
+	failNow      func()
+	failNowCalls int
+}
+
+func (t *dummyT) Helper() {
+	t.helperCalls++
+	if t.helper != nil {
+		t.helper()
+	}
+}
+
+func (t *dummyT) Error(args ...any) {
+	t.errorCalls++
+	if t.error != nil {
+		t.error(args...)
+	}
+}
+
+func (t *dummyT) Errorf(format string, args ...any) {
+	t.errorfCalls++
+	if t.errorf != nil {
+		t.errorf(format, args...)
+	}
+}
+
+func (t *dummyT) FailNow() {
+	t.failNowCalls++
+	if t.failNow != nil {
+		t.failNow()
+	}
+}
