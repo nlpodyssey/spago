cc73e292cbcf4f4ca4b74dfc61ba5de673bcecb7
Marco Nicola <marconicola@disroot.org>
2022-04-18 17:54:46 +0200

    Remove unused reflection functions from "utils"

--- utils/refl.go
+++ utils/refl.go
@@ -6,15 +6,6 @@ package utils
 
 import "reflect"
 
-// ReverseInPlace reverses the elements of s (usually a slice).
-func ReverseInPlace(s any) {
-	n := reflect.ValueOf(s).Len()
-	swap := reflect.Swapper(s)
-	for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
-		swap(i, j)
-	}
-}
-
 // ForEachField calls the callback for each field of the struct i.
 func ForEachField(i any, callback func(field any, name string, tag reflect.StructTag)) {
 	v := reflect.ValueOf(i)
@@ -36,27 +27,4 @@ func ForEachField(i any, callback func(field any, name string, tag reflect.Struc
 			callback(vField.Interface(), tField.Name, tField.Tag)
 		}
 	}
-}
-
-// TypeName returns the type name of instance.
-func TypeName(instance any) string {
-	t := reflect.TypeOf(instance)
-	if t.Kind() == reflect.Ptr {
-		return t.Elem().Name()
-	}
-	return t.Name()
-}
-
-// Name returns the name of i.
-func Name(i any) string {
-	if IsStruct(i) {
-		return reflect.TypeOf(i).String()
-	}
-	//pointer
-	return reflect.TypeOf(i).Elem().String()
-}
-
-// IsStruct returns wether i is a struct.
-func IsStruct(i any) bool {
-	return reflect.ValueOf(i).Kind() == reflect.Struct
 }
