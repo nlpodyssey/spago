b12e7ba87f06500b64262fa600bc0fbe6415854b
Marco Nicola <marconicola@disroot.org>
2022-05-04 18:54:49 +0200

    Create package "encoding/dot"

---
+++ encoding/dot/dot.go
@@ -0,0 +1,99 @@
+// Copyright 2022 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package dot allows exporting an encoding.Graph to Graphviz DOT format.
+package dot
+
+import (
+	"io"
+	"strconv"
+	"text/template"
+
+	"github.com/nlpodyssey/spago/ag"
+	"github.com/nlpodyssey/spago/encoding"
+	"github.com/nlpodyssey/spago/mat"
+)
+
+// Encode encodes the graph g in Graphviz DOT format, writing the output to w.
+func Encode[T mat.DType](g *encoding.Graph[T], w io.Writer) error {
+	return dotTemplate.Execute(w, g)
+}
+
+// language=gotemplate
+const templateText = `
+{{- block "graph" . -}}
+strict digraph {
+	rankdir=LR;
+	colorscheme="dark28";
+	node [colorscheme="dark28"];
+{{template "nodes" .}}
+{{template "edges" .Edges}}
+}
+{{end -}}
+
+{{- define "nodes" -}}
+	{{- if (le (len .NodesByTimeStep) 1) -}}
+		{{- template "singleCluster" .NodesList -}}
+	{{- else -}}
+		{{- template "multiCluster" . -}}
+	{{- end -}}
+{{- end -}}
+
+{{- define "singleCluster" -}}
+	{{- range $i, $n := .}}
+	{{printf 
+		"%d [label=<<sup>%d</sup><br/><b>%s</b><br/><sub>%d×%d</sub>>,shape=%s]" 
+		$i $i (.Name | html) .Value.Rows .Value.Columns (nodeShape $n)
+	}}
+	{{- end -}}
+{{- end -}}
+
+{{- define "multiCluster" -}}
+	{{- range $timeStep, $nodeIDs := .NodesByTimeStep}}
+	subgraph "cluster_timestep_{{$timeStep}}" {
+		label="Time Step {{$timeStep}}";
+		{{- $color := (timeStepColor $timeStep)}}
+		color={{$color}};
+		node [color={{$color}}];
+		{{range $nodeIDs}}
+			{{- $node := (index $.NodesList .)}}
+		{{printf 
+			"%d [label=<<sup>%d</sup><br/><b>%s</b><br/><sub>%d×%d</sub>>,shape=%s,color=%s]" 
+			. . ($node.Name | html)
+			$node.Value.Rows $node.Value.Columns (nodeShape $node) $color
+		}}
+		{{- end}}
+	}
+{{end -}}
+{{- end -}}
+
+{{- define "edges" -}}
+	{{- range $fromNode, $toNodes := . -}}{{range $toNodes}}
+	{{$fromNode}}->{{.}};
+	{{- end -}}{{- end -}}
+{{- end -}}
+`
+
+var (
+	funcs = template.FuncMap{
+		"timeStepColor": func(timeStep int) string {
+			if timeStep < 0 {
+				return `""`
+			}
+			// dark28 color schemes has colors from 1 to 8
+			return strconv.Itoa((timeStep % 8) + 1)
+		},
+		"nodeShape": func(node any) string {
+			switch node.(type) {
+			case *ag.Operator[float32], *ag.Operator[float64]:
+				return "oval"
+			default:
+				return "box"
+			}
+		},
+	}
+	dotTemplate = template.Must(
+		template.New("DotTemplate").Funcs(funcs).Parse(templateText),
+	)
+)
---
+++ encoding/dot/dot_test.go
@@ -0,0 +1,104 @@
+// Copyright 2022 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dot_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/nlpodyssey/spago/ag"
+	"github.com/nlpodyssey/spago/encoding"
+	"github.com/nlpodyssey/spago/encoding/dot"
+	"github.com/nlpodyssey/spago/mat"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEncode(t *testing.T) {
+	t.Run("without time steps", func(t *testing.T) {
+		type T = float32
+		a := ag.NewVariableWithName[T](mat.NewScalar[T](1), false, "a")
+		b := ag.NewVariableWithName[T](mat.NewScalar[T](3), false, "b")
+		y := ag.Sum(a, b)
+
+		g := encoding.NewGraph(y)
+		buf := new(bytes.Buffer)
+		err := dot.Encode(g, buf)
+		require.NoError(t, err)
+		out := buf.String()
+
+		expectedOutput := `strict digraph {
+	rankdir=LR;
+	colorscheme="dark28";
+	node [colorscheme="dark28"];
+
+	0 [label=<<sup>0</sup><br/><b>Add</b><br/><sub>1×1</sub>>,shape=oval]
+	1 [label=<<sup>1</sup><br/><b>a</b><br/><sub>1×1</sub>>,shape=box]
+	2 [label=<<sup>2</sup><br/><b>b</b><br/><sub>1×1</sub>>,shape=box]
+
+	1->0;
+	2->0;
+}
+`
+		assert.Equal(t, expectedOutput, out)
+	})
+
+	t.Run("with time steps", func(t *testing.T) {
+		type T = float32
+		a := ag.NewVariableWithName[T](mat.NewScalar[T](1), false, "a")
+		b := ag.NewVariableWithName[T](mat.NewScalar[T](3), false, "b")
+		c := ag.NewVariableWithName[T](mat.NewScalar[T](5), false, "c")
+		d := ag.NewVariableWithName[T](mat.NewScalar[T](7), false, "d")
+		ag.SetTimeStep(c, 0)
+		ag.SetTimeStep(d, 0)
+
+		x := ag.Sum(a, b)
+		y := ag.Sum(x, c)
+		z := ag.Sum(y, d)
+
+		g := encoding.NewGraph(z)
+		buf := new(bytes.Buffer)
+		err := dot.Encode(g, buf)
+		require.NoError(t, err)
+		out := buf.String()
+
+		expectedOutput := `strict digraph {
+	rankdir=LR;
+	colorscheme="dark28";
+	node [colorscheme="dark28"];
+
+	subgraph "cluster_timestep_-1" {
+		label="Time Step -1";
+		color="";
+		node [color=""];
+		
+		2 [label=<<sup>2</sup><br/><b>Add</b><br/><sub>1×1</sub>>,shape=oval,color=""]
+		3 [label=<<sup>3</sup><br/><b>a</b><br/><sub>1×1</sub>>,shape=box,color=""]
+		4 [label=<<sup>4</sup><br/><b>b</b><br/><sub>1×1</sub>>,shape=box,color=""]
+	}
+
+	subgraph "cluster_timestep_0" {
+		label="Time Step 0";
+		color=1;
+		node [color=1];
+		
+		0 [label=<<sup>0</sup><br/><b>Add</b><br/><sub>1×1</sub>>,shape=oval,color=1]
+		1 [label=<<sup>1</sup><br/><b>Add</b><br/><sub>1×1</sub>>,shape=oval,color=1]
+		5 [label=<<sup>5</sup><br/><b>c</b><br/><sub>1×1</sub>>,shape=box,color=1]
+		6 [label=<<sup>6</sup><br/><b>d</b><br/><sub>1×1</sub>>,shape=box,color=1]
+	}
+
+
+	1->0;
+	2->1;
+	3->2;
+	4->2;
+	5->1;
+	6->0;
+}
+`
+		assert.Equal(t, expectedOutput, out)
+	})
+}
