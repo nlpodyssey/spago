e87199b66ed108f3ab2ba652bac303192ee73398
Marco Nicola <marconicola@disroot.org>
2022-05-14 17:25:12 +0200

    Add some useful methods to mat.FloatSliceInterface

--- mat/floatslice.go
+++ mat/floatslice.go
@@ -4,13 +4,19 @@
 
 package mat
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // FloatSliceInterface is implemented by any value that can be resolved
 // to a slice of a type constrained by DType.
 type FloatSliceInterface interface {
 	Float32() []float32
 	Float64() []float64
+	Len() int
+	Equals(other FloatSliceInterface) bool
+	InDelta(other FloatSliceInterface, delta float64) bool
 }
 
 // FloatSlice converts a concrete slice of DType values to an internal
@@ -43,6 +49,55 @@ func (fs floatSlice[T]) Float32() []float32 {
 // Float64 returns the value as []float64, converting it if necessary.
 func (fs floatSlice[T]) Float64() []float64 {
 	return convertFloatSlice[T, float64](fs)
+}
+
+// Len returns the length of the slice.
+func (fs floatSlice[_]) Len() int {
+	return len(fs)
+}
+
+// Equals reports whether the content of the receiver is equal to the
+// content of the other slice.
+// The data type of the other slice is converted to the same type of
+// the receiver, if necessary.
+func (fs floatSlice[T]) Equals(other FloatSliceInterface) bool {
+	l := other.Len()
+	if len(fs) != l {
+		return false
+	}
+	if l == 0 {
+		return true
+	}
+	o := DTFloatSlice[T](other)
+	_ = o[len(fs)-1]
+	for i, v := range fs {
+		if v != o[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// InDelta reports whether the receiver and the other slice have the same
+// length and all their values at the same positions are within delta.
+// The data type of the other slice is converted to the same type of
+// the receiver, if necessary.
+func (fs floatSlice[T]) InDelta(other FloatSliceInterface, delta float64) bool {
+	l := other.Len()
+	if len(fs) != l {
+		return false
+	}
+	if l == 0 {
+		return true
+	}
+	o := DTFloatSlice[T](other)
+	_ = o[len(fs)-1]
+	for i, v := range fs {
+		if math.Abs(float64(v-o[i])) > delta {
+			return false
+		}
+	}
+	return true
 }
 
 // convertFloatSlice converts the given source slice to the destination type
--- mat/floatslice_test.go
+++ mat/floatslice_test.go
@@ -59,10 +59,119 @@ func TestDTFloatSlice(t *testing.T) {
 	})
 }
 
+func TestFloatSliceImplementation_Len(t *testing.T) {
+	t.Run("float32", testFloatSliceImplLen[float32])
+	t.Run("float64", testFloatSliceImplLen[float64])
+}
+
+func testFloatSliceImplLen[T mat.DType](t *testing.T) {
+	testCases := []struct {
+		v []T
+		l int
+	}{
+		{nil, 0},
+		{[]T{}, 0},
+		{[]T{1}, 1},
+		{[]T{1, 2}, 2},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%#v", tc.v), func(t *testing.T) {
+			fs := mat.FloatSlice[T](tc.v)
+			assert.Equal(t, tc.l, fs.Len())
+		})
+	}
+}
+
+func TestFloatSliceImplementation_Equals(t *testing.T) {
+	t.Run("float32-float32", testFloatSliceImplEquals[float32, float32])
+	t.Run("float32-float64", testFloatSliceImplEquals[float32, float64])
+	t.Run("float64-float32", testFloatSliceImplEquals[float64, float32])
+	t.Run("float64-float64", testFloatSliceImplEquals[float64, float64])
+}
+
+func testFloatSliceImplEquals[A, B mat.DType](t *testing.T) {
+	testCases := []struct {
+		a []A
+		b []B
+		e bool
+	}{
+		{nil, nil, true},
+		{nil, []B{}, true},
+		{[]A{}, []B{}, true},
+		{[]A{1}, []B{1}, true},
+		{[]A{1, 2}, []B{1, 2}, true},
+		{[]A{1}, nil, false},
+		{[]A{1}, []B{}, false},
+		{[]A{1}, []B{2}, false},
+		{[]A{1}, []B{1, 2}, false},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%#v equals %#v", tc.a, tc.b), func(t *testing.T) {
+			a := mat.FloatSlice[A](tc.a)
+			b := mat.FloatSlice[B](tc.b)
+			assert.Equal(t, tc.e, a.Equals(b))
+			assert.Equal(t, tc.e, b.Equals(a))
+		})
+	}
+}
+
+func TestFloatSliceImplementation_InDelta(t *testing.T) {
+	t.Run("float32-float32", testFloatSliceImplInDelta[float32, float32])
+	t.Run("float32-float64", testFloatSliceImplInDelta[float32, float64])
+	t.Run("float64-float32", testFloatSliceImplInDelta[float64, float32])
+	t.Run("float64-float64", testFloatSliceImplInDelta[float64, float64])
+}
+
+func testFloatSliceImplInDelta[A, B mat.DType](t *testing.T) {
+	testCases := []struct {
+		a []A
+		b []B
+		d float64
+		e bool
+	}{
+		{nil, nil, 0, true},
+		{nil, []B{}, 0, true},
+		{[]A{}, []B{}, 0, true},
+		{[]A{1}, []B{1}, 0, true},
+		{[]A{1, 2}, []B{1, 2}, 0, true},
+
+		{nil, nil, 1, true},
+		{nil, []B{}, 1, true},
+		{[]A{}, []B{}, 1, true},
+		{[]A{1}, []B{1}, 1, true},
+		{[]A{1, 2}, []B{1, 2}, 1, true},
+
+		{[]A{1}, nil, 0, false},
+		{[]A{1}, []B{}, 0, false},
+		{[]A{1}, []B{2}, 0, false},
+		{[]A{1}, []B{1, 2}, 0, false},
+
+		{[]A{1}, nil, 1, false},
+		{[]A{1}, []B{}, 1, false},
+		{[]A{1}, []B{3}, 1, false},
+		{[]A{1}, []B{1, 3}, 1, false},
+
+		{[]A{1}, []B{2}, 1, true},
+		{[]A{1, 2}, []B{0, 1}, 1, true},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%#v equals %#v", tc.a, tc.b), func(t *testing.T) {
+			a := mat.FloatSlice[A](tc.a)
+			b := mat.FloatSlice[B](tc.b)
+			assert.Equal(t, tc.e, a.InDelta(b, tc.d))
+			assert.Equal(t, tc.e, b.InDelta(a, tc.d))
+		})
+	}
+}
+
 type fakeFloatSlice struct {
 	f32 []float32
 	f64 []float64
+	ln  int
 }
 
-func (f fakeFloatSlice) Float32() []float32 { return f.f32 }
-func (f fakeFloatSlice) Float64() []float64 { return f.f64 }
+func (f fakeFloatSlice) Float32() []float32                            { return f.f32 }
+func (f fakeFloatSlice) Float64() []float64                            { return f.f64 }
+func (f fakeFloatSlice) Len() int                                      { return f.ln }
+func (f fakeFloatSlice) Equals(mat.FloatSliceInterface) bool           { panic("not implemented") }
+func (f fakeFloatSlice) InDelta(mat.FloatSliceInterface, float64) bool { panic("not implemented") }
