b1015c1bc712ed16d22b109989e7187dd90aa16e
matteo-grella <matteogrella@gmail.com>
2023-03-30 13:40:31 +0200

    Revise the marshalling of dense data to include the requirement for gradient computation

--- mat/dense_marshaling.go
+++ mat/dense_marshaling.go
@@ -48,17 +48,19 @@ func (d *Dense[T]) UnmarshalBinary(data []byte) error {
 
 // Dense matrix - float32 marshaling:
 // - 1 byte - identifier binaryDenseFloat32 (byte)
+// - 1 byte - requiresGrad (bool)
 // - 8 bytes - rows (uint64)
 // - 8 bytes - cols (uint64)
 // - 4*size bytes - data (float32 as uint32-bits)
 
 func (d Dense[T]) marshalBinaryFloat32() ([]byte, error) {
-	data := make([]byte, 17+len(d.data)*4)
+	data := make([]byte, 18+len(d.data)*4)
 	data[0] = binaryDenseFloat32
-	binary.LittleEndian.PutUint64(data[1:], uint64(d.rows))
-	binary.LittleEndian.PutUint64(data[9:], uint64(d.cols))
+	data[1] = boolToByte(d.requiresGrad)
+	binary.LittleEndian.PutUint64(data[2:], uint64(d.rows))
+	binary.LittleEndian.PutUint64(data[10:], uint64(d.cols))
 
-	s := data[17:]
+	s := data[18:]
 	for _, v := range d.data {
 		binary.LittleEndian.PutUint32(s, math.Float32bits(float32(v)))
 		s = s[4:]
@@ -67,17 +69,28 @@ func (d Dense[T]) marshalBinaryFloat32() ([]byte, error) {
 	return data, nil
 }
 
+func boolToByte(b bool) byte {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func byteToBool(b byte) bool {
+	return b == 1
+}
+
 func (d *Dense[T]) unmarshalBinaryFloat32(data []byte) error {
 	if data[0] != binaryDenseFloat32 {
 		return errors.New("mat: cannot unmarshal Dense[float32]: invalid identifier")
 	}
-
-	d.rows = int(binary.LittleEndian.Uint64(data[1:]))
-	d.cols = int(binary.LittleEndian.Uint64(data[9:]))
+	d.requiresGrad = byteToBool(data[1])
+	d.rows = int(binary.LittleEndian.Uint64(data[2:]))
+	d.cols = int(binary.LittleEndian.Uint64(data[10:]))
 	size := d.rows * d.cols
 	d.data = make([]T, size)
 
-	data = data[17:]
+	data = data[18:]
 	dData := d.data
 	for i := range dData {
 		dData[i] = T(math.Float32frombits(binary.LittleEndian.Uint32(data)))
@@ -90,17 +103,19 @@ func (d *Dense[T]) unmarshalBinaryFloat32(data []byte) error {
 
 // Dense matrix - float64 marshaling:
 // - 1 byte - identifier binaryDenseFloat64 (byte)
+// - 1 byte - requiresGrad (bool)
 // - 8 bytes - rows (uint64)
 // - 8 bytes - cols (uint64)
 // - 8*size bytes - data (float64 as uint64-bits)
 
 func (d Dense[T]) marshalBinaryFloat64() ([]byte, error) {
-	data := make([]byte, 17+len(d.data)*8)
+	data := make([]byte, 18+len(d.data)*8)
 	data[0] = binaryDenseFloat64
-	binary.LittleEndian.PutUint64(data[1:], uint64(d.rows))
-	binary.LittleEndian.PutUint64(data[9:], uint64(d.cols))
+	data[1] = boolToByte(d.requiresGrad)
+	binary.LittleEndian.PutUint64(data[2:], uint64(d.rows))
+	binary.LittleEndian.PutUint64(data[10:], uint64(d.cols))
 
-	s := data[17:]
+	s := data[18:]
 	for _, v := range d.data {
 		binary.LittleEndian.PutUint64(s, math.Float64bits(float64(v)))
 		s = s[8:]
@@ -113,13 +128,13 @@ func (d *Dense[T]) unmarshalBinaryFloat64(data []byte) error {
 	if data[0] != binaryDenseFloat64 {
 		return errors.New("mat: cannot unmarshal Dense[float64]: invalid identifier")
 	}
-
-	d.rows = int(binary.LittleEndian.Uint64(data[1:]))
-	d.cols = int(binary.LittleEndian.Uint64(data[9:]))
+	d.requiresGrad = byteToBool(data[1])
+	d.rows = int(binary.LittleEndian.Uint64(data[2:]))
+	d.cols = int(binary.LittleEndian.Uint64(data[10:]))
 	size := d.rows * d.cols
 	d.data = make([]T, size)
 
-	data = data[17:]
+	data = data[18:]
 	dData := d.data
 	for i := range dData {
 		dData[i] = T(math.Float64frombits(binary.LittleEndian.Uint64(data)))
